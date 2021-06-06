88db67a68204f0205e1089048039a96061160155
Hamster <hamster620@gmail.com>
2021-06-06 08:55:42 +0800

    Add InternalStateCorruptedException.

---
+++ Core/InternalStateCorruptedException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CarinaStudio
+{
+	/// <summary>
+	/// <see cref="Exception"/> indicate that internal state is corrupted.
+	/// </summary>
+	public class InternalStateCorruptedException : Exception
+	{
+		/// <summary>
+		/// Initialize new <see cref="InternalStateCorruptedException"/> instance.
+		/// </summary>
+		public InternalStateCorruptedException()
+		{ }
+
+
+		/// <summary>
+		/// Initialize new <see cref="InternalStateCorruptedException"/> instance.
+		/// </summary>
+		/// <param name="message">Message.</param>
+		public InternalStateCorruptedException(string? message) : base(message)
+		{ }
+
+
+		/// <summary>
+		/// Initialize new <see cref="InternalStateCorruptedException"/> instance.
+		/// </summary>
+		/// <param name="message">Message.</param>
+		/// <param name="innerException">Inner exception.</param>
+		public InternalStateCorruptedException(string? message, Exception innerException) : base(message, innerException)
+		{ }
+	}
+}
