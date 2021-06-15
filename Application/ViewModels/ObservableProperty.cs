fe1eb5772be60c5d692767d3c6d1b546ae1aa7ef
Hamster <hamster620@gmail.com>
2021-06-15 14:18:58 +0800

    Define ViewModel and ObservableProperty.

---
+++ Application/ViewModels/ObservableProperty.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CarinaStudio.ViewModels
+{
+	/// <summary>
+	/// Represent an observable property.
+	/// </summary>
+	public abstract class ObservableProperty
+	{
+		// Implementation of IComparer.
+		class ComparerImpl : IComparer<ObservableProperty>
+		{
+			public int Compare(ObservableProperty? x, ObservableProperty? y)
+			{
+				if (x == null)
+				{
+					if (y == null)
+						return 0;
+					return -1;
+				}
+				if (y == null)
+					return 1;
+				return (x.Id - y.Id);
+			}
+		}
+
+
+		/// <summary>
+		/// Default <see cref="IComparer{T}"/> for <see cref="ObservableProperty"/>.
+		/// </summary>
+		public static readonly IComparer<ObservableProperty> Comparer = new ComparerImpl();
+
+
+		// Static fields.
+		volatile int nextId;
+
+
+		// Constructor.
+		internal ObservableProperty(Type ownerType, string name, Type valueType)
+		{
+			this.Id = Interlocked.Increment(ref nextId);
+			this.Name = name;
+			this.OwnerType = ownerType;
+			this.ValueType = valueType;
+		}
+
+
+		/// <summary>
+		/// Get hash-code.
+		/// </summary>
+		/// <returns>Hash-code.</returns>
+		public override int GetHashCode() => this.Id;
+
+
+		/// <summary>
+		/// Unique ID of property.
+		/// </summary>
+		public int Id { get; }
+
+
+		/// <summary>
+		/// Name of property.
+		/// </summary>
+		public string Name { get; }
+
+
+		/// <summary>
+		/// Type of property owner.
+		/// </summary>
+		public Type OwnerType { get; }
+
+
+#pragma warning disable CS8601
+		/// <summary>
+		/// Register a typed property.
+		/// </summary>
+		/// <typeparam name="TOwner">Type of owner.</typeparam>
+		/// <typeparam name="TValue">Type of value.</typeparam>
+		/// <param name="name">Name of property.</param>
+		/// <param name="defaultValue">Default value.</param>
+		/// <param name="coerce">Coercion function.</param>
+		/// <param name="validate">Validation function.</param>
+		/// <returns></returns>
+		public static ObservableProperty<TValue> Register<TOwner, TValue>(string name, TValue defaultValue = default, Func<TValue, TValue>? coerce = null, Predicate<TValue>? validate = null)
+		{
+			return new ObservableProperty<TValue>(typeof(TOwner), name, defaultValue, coerce, validate);
+		}
+#pragma warning restore CS8601
+
+
+		/// <summary>
+		/// Type of value.
+		/// </summary>
+		public Type ValueType { get; }
+	}
+
+
+	/// <summary>
+	/// Represent a typed observable property.
+	/// </summary>
+	/// <typeparam name="T">Type of value.</typeparam>
+	public class ObservableProperty<T> : ObservableProperty
+	{
+		// Constructor.
+		internal ObservableProperty(Type ownerType, string name, T defaultValue, Func<T, T>? coerce, Predicate<T>? validate) : base(ownerType, name, typeof(T))
+		{
+			this.DefaultValue = defaultValue;
+			this.CoercionFunction = coerce ?? DefaultCoercionFunction;
+			this.ValidationFunction = validate ?? DefaultValidationFunction;
+		}
+
+
+		/// <summary>
+		/// Coercion function.
+		/// </summary>
+		public Func<T, T> CoercionFunction { get; }
+
+
+		// Default coercion function.
+		static T DefaultCoercionFunction(T value) => value;
+
+
+		// Default validation function.
+		static bool DefaultValidationFunction(T value) => true;
+
+
+		/// <summary>
+		/// Default value.
+		/// </summary>
+		public T DefaultValue { get; }
+
+
+		/// <summary>
+		/// Validation function.
+		/// </summary>
+		public Predicate<T> ValidationFunction { get; }
+	}
+}
---
+++ Application/ViewModels/ViewModel.cs
@@ -0,0 +1,152 @@
+﻿using CarinaStudio.Threading;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+
+namespace CarinaStudio.ViewModels
+{
+	/// <summary>
+	/// Base class for view-model.
+	/// </summary>
+	public abstract class ViewModel : BaseDisposable, IApplicationObject, INotifyPropertyChanged
+	{
+		// Fields.
+		readonly Thread dependencyThread;
+		readonly SortedList<ObservableProperty, object?> propertyValues = new SortedList<ObservableProperty, object?>(ObservableProperty.Comparer);
+
+
+		/// <summary>
+		/// Initialize new <see cref="ViewModel"/> instance.
+		/// </summary>
+		/// <param name="app"><see cref="IApplication"/> which view-model belongs to.</param>
+		protected ViewModel(IApplication app)
+		{
+			app.VerifyAccess();
+			this.Application = app;
+			this.Logger = app.LoggerFactory.CreateLogger(this.GetType().Name);
+			this.SynchronizationContext = SynchronizationContext.Current ?? throw new InvalidOperationException("No SynchronizationContext on current thread.");
+			this.dependencyThread = Thread.CurrentThread;
+		}
+
+
+		/// <summary>
+		/// Get <see cref="IApplication"/> which view-model belongs to.
+		/// </summary>
+		public IApplication Application { get; }
+
+
+		/// <summary>
+		/// Check whether current thread is the thread which view-model depends on or not.
+		/// </summary>
+		/// <returns>True if current thread is the thread which view-model depends on.</returns>
+		public bool CheckAccess() => Thread.CurrentThread == this.dependencyThread;
+
+
+		// Check equality of two values.
+		bool CheckValueEuqality(object? x, object? y)
+		{
+			if (x != null)
+				return x.Equals(y);
+			return y == null;
+		}
+
+
+		/// <summary>
+		/// Dispose the view-model.
+		/// </summary>
+		/// <param name="disposing">True to release managed resources.</param>
+		protected override void Dispose(bool disposing)
+		{
+			this.PropertyChanged = null;
+		}
+
+
+#pragma warning disable CS8603
+		/// <summary>
+		/// Get property value.
+		/// </summary>
+		/// <typeparam name="T">Type of value.</typeparam>
+		/// <param name="property">Property to get.</param>
+		/// <returns>Value of property.</returns>
+		protected T GetValue<T>(ObservableProperty<T> property)
+		{
+			this.VerifyAccess();
+			if (this.propertyValues.TryGetValue(property, out var value))
+				return (T)value;
+			return property.DefaultValue;
+		}
+#pragma warning restore CS8603
+
+
+		/// <summary>
+		/// Logger of this instance.
+		/// </summary>
+		protected ILogger Logger { get; }
+
+
+		/// <summary>
+		/// Called when property changed.
+		/// </summary>
+		/// <param name="property">Changed property.</param>
+		/// <param name="oldValue">Old value.</param>
+		/// <param name="newValue">New value.</param>
+		protected virtual void OnPropertyChanged(ObservableProperty property, object? oldValue, object? newValue)
+		{
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property.Name));
+		}
+
+
+		/// <summary>
+		/// Raised when property changed.
+		/// </summary>
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+
+		/// <summary>
+		/// Set property value.
+		/// </summary>
+		/// <typeparam name="T">Type of value.</typeparam>
+		/// <param name="property">Property to set.</param>
+		/// <param name="value">Value.</param>
+		protected void SetValue<T>(ObservableProperty<T> property, T value)
+		{
+			// check state
+			this.VerifyAccess();
+			this.ThrowIfDisposed();
+
+			// check owner
+			if (!property.OwnerType.IsAssignableFrom(this.GetType()))
+				throw new ArgumentException($"{this.GetType().Name} is not owner of property '{property.Name}'.");
+
+			// coerce value
+			value = property.CoercionFunction(value);
+
+			// validate value
+			if (!property.ValidationFunction(value))
+				throw new ArgumentException($"Invalid value for property '{property.Name}': {value}.");
+
+			// get old value
+			if (!this.propertyValues.TryGetValue(property, out var oldValue))
+				oldValue = property.DefaultValue;
+
+			// check equality
+			if (this.CheckValueEuqality(oldValue, value))
+				return;
+
+			// update value
+			if (this.CheckValueEuqality(property.DefaultValue, value))
+				this.propertyValues.Remove(property);
+			else
+				this.propertyValues[property] = value;
+			this.OnPropertyChanged(property, oldValue, value);
+		}
+
+
+		/// <summary>
+		/// Get <see cref="SynchronizationContext"/> on thread which view-model depends on.
+		/// </summary>
+		public SynchronizationContext SynchronizationContext { get; }
+	}
+}
