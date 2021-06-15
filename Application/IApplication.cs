a08b0a2d2de5fe4ca0e20820f585bda0909a43d0
Hamster <hamster620@gmail.com>
2021-06-15 13:28:16 +0800

    Define IApplication interface.

---
+++ Application/IApplication.cs
@@ -0,0 +1,24 @@
+﻿using CarinaStudio.Configuration;
+using CarinaStudio.Threading;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CarinaStudio
+{
+	/// <summary>
+	/// Application interface. This is the top-level object for application infrastructure.
+	/// </summary>
+	public interface IApplication : IThreadDependent
+	{
+		/// <summary>
+		/// <see cref="ILoggerFactory"/> to create logger.
+		/// </summary>
+		ILoggerFactory LoggerFactory { get; }
+
+
+		/// <summary>
+		/// Get default application level settings.
+		/// </summary>
+		BaseSettings Settings { get; }
+	}
+}
---
+++ Application/IApplicationObject.cs
@@ -0,0 +1,16 @@
+﻿using CarinaStudio.Threading;
+using System;
+
+namespace CarinaStudio
+{
+	/// <summary>
+	/// Object which belongs to an <see cref="IApplication"/>.
+	/// </summary>
+	public interface IApplicationObject : IThreadDependent
+	{
+		/// <summary>
+		/// Get <see cref="IApplication"/> which object belongs to.
+		/// </summary>
+		IApplication Application { get; }
+	}
+}
