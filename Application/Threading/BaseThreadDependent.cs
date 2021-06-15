496c3c025017b279c01b8a27c1bddf3adb63ff3e
Hamster <hamster620@gmail.com>
2021-06-15 11:21:06 +0800

    Add new project "Application" for desktop application infrastructure.

---
+++ Application/Threading/BaseThreadDependent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace CarinaStudio.Threading
+{
+	/// <summary>
+	/// Base implementation of <see cref="IThreadDependent"/>.
+	/// </summary>
+	public abstract class BaseThreadDependent : IThreadDependent
+	{
+		// Fields.
+		readonly Thread dependencyThread;
+
+
+		/// <summary>
+		/// Initialize new <see cref="BaseThreadDependent"/> instance.
+		/// </summary>
+		protected BaseThreadDependent()
+		{
+			this.SynchronizationContext = SynchronizationContext.Current ?? throw new InvalidOperationException("There is no SynchronizationContext on current thread.");
+			this.dependencyThread = Thread.CurrentThread;
+		}
+
+
+		/// <summary>
+		/// Check whether current thread is the thread which object depends on or not.
+		/// </summary>
+		/// <returns>True if current thread is the thread which object depends on.</returns>
+		public bool CheckAccess() => Thread.CurrentThread == this.dependencyThread;
+
+
+		/// <summary>
+		/// Get <see cref="SynchronizationContext"/> relates to the thread which instance depends on.
+		/// </summary>
+		public SynchronizationContext SynchronizationContext { get; }
+	}
+}
---
+++ Application/Threading/IThreadDependent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CarinaStudio.Threading
+{
+	/// <summary>
+	/// Object which depends on specific thread.
+	/// </summary>
+	public interface IThreadDependent : ISynchronizable
+	{
+		/// <summary>
+		/// Check whether current thread is the thread which object depends on or not.
+		/// </summary>
+		/// <returns>True if current thread is the thread which object depends on.</returns>
+		bool CheckAccess();
+	}
+
+
+	/// <summary>
+	/// Extensions for <see cref="IThreadDependent"/>.
+	/// </summary>
+	public static class ThreadDependentExtensions
+	{
+		/// <summary>
+		/// Throw <see cref="InvalidOperationException"/> if current thread is not the thread which object depends on.
+		/// </summary>
+		/// <param name="threadDependent"><see cref="IThreadDependent"/>.</param>
+		public static void VerifyAccess(this IThreadDependent threadDependent)
+		{
+			if (!threadDependent.CheckAccess())
+				throw new InvalidOperationException("Access the object from the thread which is not the object depends on.");
+		}
+	}
+}
