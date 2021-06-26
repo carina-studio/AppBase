baa9c714dc627235125b226ccf9556c9475c0aff
Hamster <hamster620@gmail.com>
2021-06-26 20:44:48 +0800

    Add ObservableList and its tests.

---
+++ Core.Tests/Collections/ObservableListTests.cs
@@ -0,0 +1,102 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace CarinaStudio.Collections
+{
+	/// <summary>
+	/// Tests of <see cref="ObservableList{T}"/>.
+	/// </summary>
+	[TestFixture]
+	class ObservableListTests
+	{
+		// Fields.
+		readonly Random random = new Random();
+
+
+		/// <summary>
+		/// Test for collection changed event.
+		/// </summary>
+		[Test]
+		public void CollectionChangedEventTest()
+		{
+			// prepare
+			var list = new ObservableList<int>();
+			var reflectedList = new List<int>();
+			list.CollectionChanged += (_, e) =>
+			{
+				switch (e.Action)
+				{
+					case NotifyCollectionChangedAction.Add:
+						{
+							var newItems = e.NewItems;
+							var elements = new int[newItems.Count].Also((it) => newItems.CopyTo(it, 0));
+							reflectedList.InsertRange(e.NewStartingIndex, elements);
+						}
+						break;
+					case NotifyCollectionChangedAction.Remove:
+						reflectedList.RemoveRange(e.OldStartingIndex, e.OldItems.Count);
+						break;
+					case NotifyCollectionChangedAction.Reset:
+						reflectedList.Clear();
+						reflectedList.AddRange(list);
+						break;
+					default:
+						throw new AssertionException($"Uncovered collection change action: {e.Action}.");
+				}
+			};
+
+			// add random elements
+			for (var i = 0; i < 100; ++i)
+				list.Add(this.random.Next());
+			Assert.IsTrue(list.SequenceEqual(reflectedList), "List built by collection change event is incorrect after adding elements.");
+			for (var i = 0; i < 100; ++i)
+			{
+				var count = this.random.Next(1, 101);
+				var elements = this.GenerateRandomArray(count);
+				list.AddRange(elements);
+			}
+			Assert.IsTrue(list.SequenceEqual(reflectedList), "List built by collection change event is incorrect after adding elements.");
+
+			// insert random elements
+			for (var i = 0; i < 100; ++i)
+				list.Insert(this.random.Next(list.Count + 1), this.random.Next());
+			Assert.IsTrue(list.SequenceEqual(reflectedList), "List built by collection change event is incorrect after inserting elements.");
+			for (var i = 0; i < 100; ++i)
+			{
+				var count = this.random.Next(1, 101);
+				var elements = this.GenerateRandomArray(count);
+				list.InsertRange(this.random.Next(list.Count + 1), elements);
+			}
+			Assert.IsTrue(list.SequenceEqual(reflectedList), "List built by collection change event is incorrect after inserting elements.");
+
+			// remove random elements
+			for (var i = 0; i < 100; ++i)
+				list.RemoveAt(this.random.Next(list.Count));
+			Assert.IsTrue(list.SequenceEqual(reflectedList), "List built by collection change event is incorrect after removing elements.");
+			for (var i = 0; i < 100; ++i)
+			{
+				var index = this.random.Next(list.Count - 1);
+				var count = this.random.Next(1, 11);
+				if (index + count > list.Count)
+					count = list.Count - index;
+				list.RemoveRange(index, count);
+			}
+			Assert.IsTrue(list.SequenceEqual(reflectedList), "List built by collection change event is incorrect after removing elements.");
+
+			// Clear
+			list.Clear();
+			Assert.IsTrue(list.SequenceEqual(reflectedList), "List built by collection change event is incorrect after clearing.");
+		}
+
+
+		// Generate array with random value.
+		int[] GenerateRandomArray(int count) => new int[count].Also((it) =>
+		{
+			for (var i = count - 1; i >= 0; --i)
+				it[i] = this.random.Next(0, count / 2);
+		});
+	}
+}
---
+++ Core/Collections/ObservableList.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace CarinaStudio.Collections
+{
+	/// <summary>
+	/// Implementation of <see cref="IList{T}"/> which also implements <see cref="INotifyCollectionChanged"/>.
+	/// </summary>
+	/// <typeparam name="T">Type of element.</typeparam>
+	public class ObservableList<T> : IList, IList<T>, INotifyCollectionChanged, IReadOnlyList<T>
+	{
+		// Fields.
+		readonly List<T> list;
+
+
+		/// <summary>
+		/// Initialize new <see cref="ObservableList{T}"/> instance.
+		/// </summary>
+		/// <param name="elements">Initial elements.</param>
+		public ObservableList(IEnumerable<T>? elements = null)
+		{
+			if (elements == null)
+				this.list = new List<T>();
+			else
+				this.list = new List<T>(elements);
+		}
+
+
+		/// <summary>
+		/// Initialize new <see cref="ObservableList{T}"/> instance.
+		/// </summary>
+		/// <param name="capacity">Initial capacity.</param>
+		public ObservableList(int capacity)
+		{
+			this.list = new List<T>(capacity);
+		}
+
+
+		/// <summary>
+		/// Add element to list.
+		/// </summary>
+		/// <param name="element">Element to add.</param>
+		/// <returns>Index of added element in list.</returns>
+		public int Add(T element)
+		{
+			var index = this.list.Count;
+			this.list.Add(element);
+			this.CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, element, index));
+			return index;
+		}
+
+
+		/// <summary>
+		/// Add elements to list.
+		/// </summary>
+		/// <param name="elements">Elements to add.</param>
+		public void AddRange(IEnumerable<T> elements)
+		{
+			var index = this.list.Count;
+			this.list.AddRange(elements);
+			this.CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, ToIList(elements), index));
+		}
+
+
+		/// <summary>
+		/// Clear all elements.
+		/// </summary>
+		public void Clear()
+		{
+			this.list.Clear();
+			this.CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+		}
+
+
+		/// <summary>
+		/// Raised when list changed.
+		/// </summary>
+		public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+
+		/// <summary>
+		/// Check whether given element is contained in list or not.
+		/// </summary>
+		/// <param name="element">Element.</param>
+		/// <returns>True if element is contained in list.</returns>
+		public bool Contains(T element) => this.list.Contains(element);
+
+
+		/// <summary>
+		/// Copy elements to array.
+		/// </summary>
+		/// <param name="array">Array to place copied elements.</param>
+		/// <param name="arrayIndex">Index of first position in <paramref name="array"/> to place copied elements.</param>
+		public void CopyTo(T[] array, int arrayIndex) => this.list.CopyTo(array, arrayIndex);
+
+
+		/// <summary>
+		/// Copy elements to array.
+		/// </summary>
+		/// <param name="index">Index of first element in list to copy.</param>
+		/// <param name="array">Array to place copied elements.</param>
+		/// <param name="arrayIndex">Index of first position in <paramref name="array"/> to place copied elements.</param>
+		/// <param name="count">Number of elements to copy.</param>
+		public void CopyTo(int index, T[] array, int arrayIndex, int count) => this.list.CopyTo(index, array, arrayIndex, count);
+
+
+		/// <summary>
+		/// Get number of elements.
+		/// </summary>
+		public int Count { get => this.list.Count; }
+
+
+		/// <summary>
+		/// Get <see cref="IEnumerator{T}"/> to enumerate elements.
+		/// </summary>
+		/// <returns><see cref="IEnumerator{T}"/>.</returns>
+		public IEnumerator<T> GetEnumerator() => this.list.GetEnumerator();
+
+
+		/// <summary>
+		/// Get index of given element in list.
+		/// </summary>
+		/// <param name="element">Element.</param>
+		/// <returns>Index of element in list, or -1 if element is not contained in list.</returns>
+		public int IndexOf(T element) => this.list.IndexOf(element);
+
+
+		/// <summary>
+		/// Insert element at given position.
+		/// </summary>
+		/// <param name="index">Index of position to insert element.</param>
+		/// <param name="element">Element to insert.</param>
+		public void Insert(int index, T element)
+		{
+			this.list.Insert(index, element);
+			this.CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, element, index));
+		}
+
+
+		/// <summary>
+		/// Insert elements at given position.
+		/// </summary>
+		/// <param name="index">Index of position to insert elements.</param>
+		/// <param name="elements">Elements to insert.</param>
+		public void InsertRange(int index, IEnumerable<T> elements)
+		{
+			this.list.InsertRange(index, elements);
+			this.CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, ToIList(elements), index));
+		}
+
+
+		/// <summary>
+		/// Remove first found of given element.
+		/// </summary>
+		/// <param name="element">Element to remove.</param>
+		/// <returns>True if element has been removed.</returns>
+		public bool Remove(T element)
+		{
+			var index = this.list.IndexOf(element);
+			if (index < 0)
+				return false;
+			this.list.RemoveAt(index);
+			this.CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, element, index));
+			return true;
+		}
+
+
+		/// <summary>
+		/// Remove element at given position.
+		/// </summary>
+		/// <param name="index">Index of element to remove.</param>
+		public void RemoveAt(int index)
+		{
+			var collectionChanged = this.CollectionChanged;
+			if (collectionChanged != null)
+			{
+				var removedElement = this.list[index];
+				this.list.RemoveAt(index);
+				collectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, removedElement, index));
+			}
+			else
+				this.list.RemoveAt(index);
+		}
+
+
+		/// <summary>
+		/// Remove elements.
+		/// </summary>
+		/// <param name="index">Index of first element to remove.</param>
+		/// <param name="count">Number of elements to remove.</param>
+		public void RemoveRange(int index, int count)
+		{
+			var collectionChanged = this.CollectionChanged;
+			if (collectionChanged != null)
+			{
+				var removedElements = this.list.ToArray(index, count);
+				this.list.RemoveRange(index, count);
+				collectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, removedElements, index));
+			}
+			else
+				this.list.RemoveRange(index, count);
+		}
+
+
+		/// <summary>
+		/// Get or set element.
+		/// </summary>
+		/// <param name="index">Index of element.</param>
+		/// <returns>Element.</returns>
+		public T this[int index] 
+		{
+			get => this.list[index];
+			set => this.list[index] = value;
+		}
+
+
+		/// <summary>
+		/// Copy elements to array.
+		/// </summary>
+		/// <returns>Array of copied elements</returns>
+		public T[] ToArray() => this.list.ToArray();
+
+
+		/// <summary>
+		/// Copy elements to array.
+		/// </summary>
+		/// <param name="index">Index of first element in list to copy.</param>
+		/// <param name="count">Number of elements to copy.</param>
+		/// <returns>Array of copied elements</returns>
+		public T[] ToArray(int index, int count) => new T[count].Also((it) => this.CopyTo(index, it, 0, count));
+
+
+		// Convert elements to IList.
+		static IList ToIList(IEnumerable<T> elements)
+		{
+			if (elements is IList list)
+				return list;
+			return elements.ToArray();
+		}
+
+
+		/// <summary>
+		/// Set the capacity to the actual number of elements in list.
+		/// </summary>
+		public void TrimExcess() => this.list.TrimExcess();
+
+
+		// Interface implementations.
+		void ICollection<T>.Add(T value) => this.Add(value);
+		void ICollection.CopyTo(Array array, int index) => this.CopyTo((T[])array, index);
+		bool ICollection<T>.IsReadOnly => false;
+		bool ICollection.IsSynchronized => false;
+		object ICollection.SyncRoot => this;
+		IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+#pragma warning disable CS8604
+		int IList.Add(object? value) => this.Add((T)value);
+#pragma warning restore CS8604
+		bool IList.Contains(object? value)
+		{
+			if (value is T element)
+				return this.Contains(element);
+			return false;
+		}
+		int IList.IndexOf(object? value)
+		{
+			if (value is T element)
+				return this.IndexOf(element);
+			return -1;
+		}
+#pragma warning disable CS8604
+		void IList.Insert(int index, object? value) => this.Insert(index, (T)value);
+#pragma warning restore CS8604
+		bool IList.IsFixedSize => false;
+		bool IList.IsReadOnly => false;
+#pragma warning disable CS8601
+		object? IList.this[int index] 
+		{ 
+			get => this.list[index];
+			set => this.list[index] = (T)value;
+		}
+#pragma warning restore CS8601
+		void IList.Remove(object? value)
+		{
+			if (value is T element)
+				this.Remove(element);
+		}
+	}
+}
