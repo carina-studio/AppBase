0d1c590fca6eea3dcae1517fc01c47f8efa4a080
Hamster <hamster620@gmail.com>
2021-06-29 20:40:13 +0800

    [1] Add extensions for IDictionary<K, V> and ISet<T>. [2] Add ReadOnlySet<T>.

---
+++ Core/Collections/DictionaryExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CarinaStudio.Collections
+{
+	/// <summary>
+	/// Extensions for <see cref="IDictionary{TKey, TValue}"/>.
+	/// </summary>
+	public static class DictionaryExtensions
+	{
+		/// <summary>
+		/// Add all <see cref="KeyValuePair{TKey, TValue}"/>s to dictionary.
+		/// </summary>
+		/// <typeparam name="TKey">Type of key.</typeparam>
+		/// <typeparam name="TValue">Type of value.</typeparam>
+		/// <param name="dictionary"><see cref="IDictionary{TKey, TValue}"/>.</param>
+		/// <param name="keyValues"><see cref="KeyValuePair{TKey, TValue}"/>s to add.</param>
+		public static void AddAll<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, IEnumerable<KeyValuePair<TKey, TValue>> keyValues) where TKey : notnull
+		{
+			foreach (var pair in keyValues)
+				dictionary.Add(pair.Key, pair.Value);
+		}
+
+
+		/// <summary>
+		/// Make dictionary as read-only.
+		/// </summary>
+		/// <typeparam name="TKey">Type of key.</typeparam>
+		/// <typeparam name="TValue">Type of value.</typeparam>
+		/// <param name="dictionary"><see cref="IDictionary{TKey, TValue}"/>.</param>
+		/// <returns>Read-only dictionary.</returns>
+		public static IDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(this IDictionary<TKey, TValue> dictionary) where TKey : notnull
+		{
+			if (dictionary.IsReadOnly)
+				return dictionary;
+			return new ReadOnlyDictionary<TKey, TValue>(dictionary);
+		}
+	}
+}
---
+++ Core/Collections/SetExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarinaStudio.Collections
+{
+	/// <summary>
+	/// Extensions for <see cref="ISet{T}"/>.
+	/// </summary>
+	public static class SetExtensions
+	{
+		/// <summary>
+		/// Add all given elements to the set.
+		/// </summary>
+		/// <typeparam name="T">Type of element.</typeparam>
+		/// <param name="set"><see cref="ISet{T}"/>.</param>
+		/// <param name="elements">Elements to add.</param>
+		public static void AddAll<T>(this ISet<T> set, IEnumerable<T> elements)
+		{
+			foreach (var element in elements)
+				set.Add(element);
+		}
+
+
+		/// <summary>
+		/// Make <see cref="ISet{T}"/> as read-only.
+		/// </summary>
+		/// <typeparam name="T">Type of element.</typeparam>
+		/// <param name="set"><see cref="ISet{T}"/>.</param>
+		/// <returns>Read-only <see cref="ISet{T}"/>.</returns>
+		public static ISet<T> AsReadOnly<T>(this ISet<T> set)
+		{
+			if (set.IsReadOnly)
+				return set;
+			return new ReadOnlySet<T>(set);
+		}
+	}
+}
