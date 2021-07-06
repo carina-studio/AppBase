39e03ae249519f9ac1fa1bc5625ebc6f5a944fed
Hamster <hamster620@gmail.com>
2021-07-06 18:41:36 +0800

    Add extensions for ICommand.

---
+++ Application/Windows/Input/CommandExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CarinaStudio.Windows.Input
+{
+	/// <summary>
+	/// Extensions for <see cref="ICommand"/>.
+	/// </summary>
+	public static class CommandExtensions
+	{
+		/// <summary>
+		/// Check whether command can be executed and execute.
+		/// </summary>
+		/// <param name="command">Command.</param>
+		/// <param name="parameter">Command parameter.</param>
+		/// <returns>True if command can be executed and has been executed.</returns>
+		public static bool TryExecute(this ICommand command, object? parameter = null)
+		{
+			if (!command.CanExecute(parameter))
+				return false;
+			command.Execute(parameter);
+			return true;
+		}
+	}
+}
