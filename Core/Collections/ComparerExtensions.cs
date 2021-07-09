f9d08bbe4fb05fac213b97b634d20c65726c99bf
Hamster <hamster620@gmail.com>
2021-07-09 10:15:42 +0800

    Add extensions for Comparison and IComparer to build inverse comparer.

---
+++ Core/Collections/ComparerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace CarinaStudio.Collections
+{
+	/// <summary>
+	/// Extensions for <see cref="IComparer{T}"/>.
+	/// </summary>
+	public static class ComparerExtensions
+	{
+		/// <summary>
+		/// Create new <see cref="IComparer{T}"/> which generates inverse result of given <see cref="IComparer{T}"/>.
+		/// </summary>
+		/// <typeparam name="T">Type of value to compare.</typeparam>
+		/// <param name="comparer"><see cref="IComparer{T}"/>.</param>
+		/// <returns><see cref="IComparer{T}"/> which generates inverse result of given <see cref="IComparer{T}"/>.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static IComparer<T> Inverse<T>(this IComparer<T> comparer) => Comparer<T>.Create((x, y) => comparer.Compare(y, x));
+	}
+}
