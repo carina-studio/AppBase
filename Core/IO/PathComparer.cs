16f37bbc9b8a9461fd0ade480e022c62ab674cb0
Hamster <hamster620@gmail.com>
2021-07-09 10:08:24 +0800

    Add PathComparer.

---
+++ Core/IO/PathComparer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace CarinaStudio.IO
+{
+	/// <summary>
+	/// <see cref="IComparer{T}"/> to compare file paths. Case will be ignored on Windows.
+	/// </summary>
+	public class PathComparer : IComparer<string>
+	{
+		/// <summary>
+		/// Default instance.
+		/// </summary>
+		public static readonly PathComparer Default = new PathComparer();
+
+
+		// Static fields.
+		static readonly bool ignoreCases = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+
+		// Constructor.
+		PathComparer()
+		{ }
+
+
+		/// <summary>
+		/// Compare file paths.
+		/// </summary>
+		/// <param name="x">1st file path.</param>
+		/// <param name="y">2nd file path.</param>
+		/// <returns>Comparison result.</returns>
+		public int Compare(string? x, string? y) => string.Compare(x, y, ignoreCases);
+	}
+}
