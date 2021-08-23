e42959d43aafbb6cca4542344efd4755641816b3
Hamster <hamster620@gmail.com>
2021-08-23 21:01:08 +0800

    Add MemoryStreamProvider and its tests.

---
+++ Core.Tests/IO/MemoryStreamProviderTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarinaStudio.IO
+{
+	/// <summary>
+	/// Tests of <see cref="MemoryStreamProvider"/>.
+	/// </summary>
+	[TestFixture]
+	class MemoryStreamProviderTests : BaseStreamProviderTests
+	{
+		// Create instance.
+		protected override IStreamProvider CreateInstance(byte[] data) => new MemoryStreamProvider(data);
+
+
+		// Get written data.
+		protected override byte[] GetWrittenData(IStreamProvider provider) => ((MemoryStreamProvider)provider).ToByteArray();
+
+
+		/// <summary>
+		/// Test for reading from partial buffer.
+		/// </summary>
+		/// <returns></returns>
+		[Test]
+		public async Task PartialBufferReadingTest()
+		{
+			// prepare
+			var buffer = this.GenerateRandomData();
+			var firstBuffer = new byte[buffer.Length / 2].Also(it => Array.Copy(buffer, 0, it, 0, it.Length));
+			var secondBuffer = new byte[buffer.Length - firstBuffer.Length].Also(it => Array.Copy(buffer, firstBuffer.Length, it, 0, it.Length));
+			var provider = new MemoryStreamProvider(buffer, firstBuffer.Length, secondBuffer.Length, true);
+
+			// check read data
+			using (var stream = await provider.OpenStreamAsync(StreamAccess.Read))
+			{
+				var data = stream.ReadAllBytes();
+				Assert.IsTrue(data.SequenceEqual(secondBuffer));
+			}
+		}
+
+
+		/// <summary>
+		/// Test for writing data to the same buffer as reading.
+		/// </summary>
+		[Test]
+		public async Task WritingInPlaceTest()
+		{
+			// prepare
+			var data1 = this.GenerateRandomData();
+			var data2 = this.GenerateRandomData();
+			var dataBuffer = (byte[])data1.Clone();
+			var provider = new MemoryStreamProvider(dataBuffer, true);
+			Assert.IsFalse(data1.SequenceEqual(data2));
+
+			// write data
+			using (var stream = await provider.OpenStreamAsync(StreamAccess.Write))
+				stream.Write(data2);
+			Assert.IsTrue(dataBuffer.SequenceEqual(data2));
+			using (var stream = await provider.OpenStreamAsync(StreamAccess.Write))
+				stream.Write(data1);
+			Assert.IsTrue(dataBuffer.SequenceEqual(data1));
+		}
+	}
+}
---
+++ Core/IO/MemoryStreamProvider.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CarinaStudio.IO
+{
+	/// <summary>
+	/// Implementation of <see cref="IStreamProvider"/> which keeps data in memory.
+	/// </summary>
+	public class MemoryStreamProvider : IStreamProvider
+	{
+		// Stream.
+		class StreamImpl : StreamWrapper
+		{
+			// Fields.
+			readonly MemoryStream memoeyStream;
+			readonly MemoryStreamProvider provider;
+
+			// Constructor.
+			public StreamImpl(MemoryStreamProvider provider, MemoryStream stream) : base(stream)
+			{
+				this.memoeyStream = stream;
+				this.provider = provider;
+			}
+
+			// Dispose.
+			protected override void Dispose(bool disposing)
+			{
+				if (this.memoeyStream.CanWrite 
+					&& this.provider.isWritable
+					&& !this.provider.isFixedSize)
+				{
+					lock (this.provider)
+					{
+						this.provider.data = this.memoeyStream.ToArray();
+						this.provider.dataOffset = 0;
+						this.provider.dataCount = this.provider.data.Length;
+					}
+				}
+				base.Dispose(disposing);
+			}
+		}
+
+
+		// Fields.
+		volatile byte[] data;
+		volatile int dataCount;
+		volatile int dataOffset;
+		readonly bool isFixedSize;
+		readonly bool isWritable;
+
+
+		/// <summary>
+		/// Initialize new <see cref="MemoryStreamProvider"/>.
+		/// </summary>
+		public MemoryStreamProvider()
+		{
+			this.data = new byte[0];
+			this.isWritable = true;
+		}
+
+
+		/// <summary>
+		/// Initialize new <see cref="MemoryStreamProvider"/> for reading.
+		/// </summary>
+		/// <param name="buffer">Data buffer.</param>
+		public MemoryStreamProvider(byte[] buffer) : this(buffer, 0, buffer.Length, true)
+		{ }
+
+
+		/// <summary>
+		/// Initialize new <see cref="MemoryStreamProvider"/> for reading.
+		/// </summary>
+		/// <param name="buffer">Data buffer.</param>
+		/// <param name="isWritable">True if data can be write back to <paramref name="buffer"/>.</param>
+		public MemoryStreamProvider(byte[] buffer, bool isWritable) : this(buffer, 0, buffer.Length, isWritable)
+		{ }
+
+
+		/// <summary>
+		/// Initialize new <see cref="MemoryStreamProvider"/> for reading.
+		/// </summary>
+		/// <param name="buffer">Data buffer.</param>
+		/// <param name="index">Index of first byte to be read or written in <paramref name="buffer"/>.</param>
+		/// <param name="count">Number of bytes in <paramref name="buffer"/> to be read or written.</param>
+		/// <param name="isWritable">True if data can be write back to <paramref name="buffer"/>.</param>
+		public MemoryStreamProvider(byte[] buffer, int index, int count, bool isWritable)
+		{
+			if (buffer == null)
+				throw new ArgumentNullException(nameof(buffer));
+			if (index < 0 || index > buffer.Length)
+				throw new ArgumentOutOfRangeException(nameof(index));
+			if (count < 0 || index + count > buffer.Length)
+				throw new ArgumentOutOfRangeException(nameof(count));
+			this.data = buffer;
+			this.dataCount = count;
+			this.dataOffset = index;
+			this.isFixedSize = true;
+			this.isWritable = isWritable;
+		}
+
+
+		/// <summary>
+		/// Check whether given access to <see cref="Stream"/> is supported by this provider or not.
+		/// </summary>
+		/// <param name="access">Access to stream.</param>
+		/// <returns>True if given combination of access is supported.</returns>
+		public bool CheckStreamAccess(StreamAccess access)
+		{
+			if ((access & StreamAccess.Write) != 0 && !this.isWritable)
+				return false;
+			return true;
+		}
+
+
+		/// <summary>
+		/// Open stream asynchronously.
+		/// </summary>
+		/// <param name="access">Desired access to stream.</param>
+		/// <param name="token">Cancellation token.</param>
+		/// <returns>Task of opening stream.</returns>
+		public Task<Stream> OpenStreamAsync(StreamAccess access, CancellationToken token)
+		{
+			// check state
+			if (!this.CheckStreamAccess(access))
+				throw new InvalidOperationException();
+
+			// create memory stream
+			var memoryStream = this.Lock(() =>
+			{
+				return this.isFixedSize || this.data.Length > 0
+					? new MemoryStream(this.data, this.dataOffset, this.dataCount, this.isWritable)
+					: new MemoryStream();
+			});
+			return Task.Run(() => (Stream)new StreamImpl(this, memoryStream));
+		}
+
+
+		/// <summary>
+		/// Copy data as byte array.
+		/// </summary>
+		/// <returns>Byte array.</returns>
+		public byte[] ToByteArray() => this.Lock(() =>
+		{
+			if (this.dataOffset != 0 || this.dataCount != this.data.Length)
+				return new byte[this.dataCount].Also(it => Array.Copy(this.data, this.dataOffset, it, 0, this.dataCount));
+			return (byte[])this.data.Clone();
+		});
+	}
+}
