b988d4f5e4e4d7e4c5bbf561d87c9654f87c4d93
Hamster <hamster620@gmail.com>
2021-08-26 14:05:08 +0800

    Add Command which implements ICommand.

---
+++ Application/Windows/Input/BaseCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CarinaStudio.Windows.Input
+{
+	/// <summary>
+	/// Base implementation of <see cref="ICommand"/>.
+	/// </summary>
+	public abstract class BaseCommand : ICommand
+	{
+		// Fields.
+		bool canExecute = true;
+
+
+		/// <summary>
+		/// Initialize new <see cref="BaseCommand"/> instance.
+		/// </summary>
+		/// <param name="canExecute"><see cref="IObservable{T}"/> to indicate whether command can be executed or not.</param>
+		protected BaseCommand(IObservable<bool>? canExecute)
+		{
+			if (canExecute != null)
+			{
+				canExecute.Subscribe(new Observer<bool>(value =>
+				{
+					if (this.canExecute != value)
+					{
+						this.canExecute = value;
+						this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+					}
+				}));
+				if (canExecute is ObservableValue<bool> observableValue)
+					this.canExecute = observableValue.Value;
+			}
+		}
+
+
+		/// <summary>
+		/// Check whether command can be executed or not.
+		/// </summary>
+		/// <param name="parameter">Parameter.</param>
+		/// <returns></returns>
+		public bool CanExecute(object? parameter) => this.canExecute;
+
+
+		/// <summary>
+		/// Raised when result of <see cref="CanExecute(object?)"/> changed.
+		/// </summary>
+		public event EventHandler? CanExecuteChanged;
+
+
+		/// <summary>
+		/// Execute command.
+		/// </summary>
+		/// <param name="parameter">Parameter.</param>
+		public abstract void Execute(object? parameter);
+	}
+}
---
+++ Application/Windows/Input/Command.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CarinaStudio.Windows.Input
+{
+	/// <summary>
+	/// Implementation of <see cref="ICommand"/>.
+	/// </summary>
+	public class Command : BaseCommand
+	{
+		// Fields.
+		readonly Action action;
+
+
+		/// <summary>
+		/// Initialize new <see cref="Command"/> instance.
+		/// </summary>
+		/// <param name="execute">Action to execute command.</param>
+		/// <param name="canExecute"><see cref="IObservable{T}"/> to indicate whether command can be executed or not.</param>
+		public Command(Action execute, IObservable<bool>? canExecute = null) : base(canExecute)
+		{
+			this.action = execute;
+		}
+
+
+		/// <summary>
+		/// Execute command.
+		/// </summary>
+		/// <param name="parameter">Parameter.</param>
+		public override void Execute(object? parameter) => this.action();
+	}
+
+
+	/// <summary>
+	/// Implementation of <see cref="ICommand"/>.
+	/// </summary>
+	public class Command<TParam> : BaseCommand
+	{
+		// Fields.
+		readonly Action<TParam> action;
+
+
+		/// <summary>
+		/// Initialize new <see cref="Command"/> instance.
+		/// </summary>
+		/// <param name="execute">Action to execute command.</param>
+		/// <param name="canExecute"><see cref="IObservable{T}"/> to indicate whether command can be executed or not.</param>
+		public Command(Action<TParam> execute, IObservable<bool>? canExecute = null) : base(canExecute)
+		{
+			this.action = execute;
+		}
+
+
+#pragma warning disable CS8604
+		/// <summary>
+		/// Execute command.
+		/// </summary>
+		/// <param name="parameter">Parameter.</param>
+		public override void Execute(object? parameter) => this.action((TParam)parameter);
+#pragma warning restore CS8604
+	}
+}
