e701b7b3a644058b7fdfbc61951168b3ea545367
Hamster <hamster620@gmail.com>
2021-08-31 12:28:59 +0800

    Parse package manifest in background thread.

--- AutoUpdate/Resolvers/JsonPackageResolver.cs
+++ AutoUpdate/Resolvers/JsonPackageResolver.cs
@@ -31,90 +31,102 @@ namespace CarinaStudio.AutoUpdate.Resolvers
 		{
 			// get JSON data
 			using var stream = await this.Source.AsNonNull().OpenStreamAsync(StreamAccess.Read, cancellationToken);
-			using var jsonDocument = JsonDocument.Parse(stream);
-			var rootObject = jsonDocument.RootElement;
-			if (rootObject.ValueKind != JsonValueKind.Object)
-				throw new JsonException("Root element is not an object.");
 
-			// get application name
-			if (rootObject.TryGetProperty("Name", out var jsonValue)
-				&& jsonValue.ValueKind == JsonValueKind.String)
-			{
-				this.ApplicationName = jsonValue.GetString();
-			}
-
-			// get version
-			if (rootObject.TryGetProperty("Version", out jsonValue)
-				&& jsonValue.ValueKind == JsonValueKind.String
-				&& Version.TryParse(jsonValue.GetString().AsNonNull(), out var version))
-			{
-				this.PackageVersion = version;
-			}
-
-			// get page URI
-			if (rootObject.TryGetProperty("PageUri", out jsonValue)
-				&& jsonValue.ValueKind == JsonValueKind.String
-				&& Uri.TryCreate(jsonValue.GetString().AsNonNull(), UriKind.Absolute, out var pageUri))
-			{
-				this.PageUri = pageUri;
-			}
-
-			// check platform
-			var osName = Global.Run(() =>
-			{
-				if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-					return nameof(OSPlatform.Windows);
-				if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-					return nameof(OSPlatform.Linux);
-				if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-					return nameof(OSPlatform.OSX);
-				return "";
-			});
-			if (string.IsNullOrEmpty(osName))
-				throw new ArgumentException("Unknown operating system.");
-			var archName = RuntimeInformation.ProcessArchitecture.ToString();
-
-			// find package URI
-			if (!rootObject.TryGetProperty("Packages", out var jsonPackageListElement))
-				throw new ArgumentException("No packages list found.");
-			if (jsonPackageListElement.ValueKind != JsonValueKind.Array)
-				throw new ArgumentException("Package list is not an array.");
+			// parse package manifest
+			var appName = (string?)null;
+			var packageVersion = (Version?)null;
+			var pageUri = (Uri?)null;
+			var packageUri = (Uri?)null;
 			var genericPackageUri = (Uri?)null;
-			foreach (var jsonPackageElement in jsonPackageListElement.EnumerateArray())
+			await Task.Run(() =>
 			{
-				// check JSON element type
-				if (jsonPackageElement.ValueKind != JsonValueKind.Object)
-					continue;
+				// parse as JSON document
+				using var jsonDocument = JsonDocument.Parse(stream);
+				var rootObject = jsonDocument.RootElement;
+				if (rootObject.ValueKind != JsonValueKind.Object)
+					throw new JsonException("Root element is not an object.");
 
-				// get package URI
-				if (!jsonPackageElement.TryGetProperty("Uri", out jsonValue)
-					|| jsonValue.ValueKind != JsonValueKind.String
-					|| !Uri.TryCreate(jsonValue.GetString().AsNonNull(), UriKind.Absolute, out var packageUri))
+				// get application name
+				if (rootObject.TryGetProperty("Name", out var jsonValue)
+					&& jsonValue.ValueKind == JsonValueKind.String)
 				{
-					continue;
+					appName = jsonValue.GetString();
 				}
 
-				// check OS
-				var hasOsProperty = jsonPackageElement.TryGetProperty("OperatingSystem", out jsonValue);
-				var isOsMatched = (hasOsProperty && jsonValue.ValueKind == JsonValueKind.String && jsonValue.GetString() == osName);
-
-				// check CPU architecture
-				var hasArchProperty = jsonPackageElement.TryGetProperty("Architecture", out jsonValue);
-				var isArchMatched = (hasArchProperty && jsonValue.ValueKind == JsonValueKind.String && jsonValue.GetString() == archName);
-
-				// select package
-				if (!hasOsProperty && !hasArchProperty)
-					genericPackageUri = packageUri;
-				else if (isOsMatched && isArchMatched)
+				// get version
+				if (rootObject.TryGetProperty("Version", out jsonValue)
+					&& jsonValue.ValueKind == JsonValueKind.String)
 				{
-					this.PackageUri = packageUri;
-					return;
+					Version.TryParse(jsonValue.GetString().AsNonNull(), out packageVersion);
 				}
-			}
-			if (genericPackageUri != null)
-				this.PackageUri = genericPackageUri;
-			else
-				throw new ArgumentException("Package URI not found.");
+
+				// get page URI
+				if (rootObject.TryGetProperty("PageUri", out jsonValue)
+					&& jsonValue.ValueKind == JsonValueKind.String)
+				{
+					Uri.TryCreate(jsonValue.GetString().AsNonNull(), UriKind.Absolute, out pageUri);
+				}
+
+				// check platform
+				var osName = Global.Run(() =>
+				{
+					if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+						return nameof(OSPlatform.Windows);
+					if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+						return nameof(OSPlatform.Linux);
+					if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+						return nameof(OSPlatform.OSX);
+					return "";
+				});
+				if (string.IsNullOrEmpty(osName))
+					throw new ArgumentException("Unknown operating system.");
+				var archName = RuntimeInformation.ProcessArchitecture.ToString();
+
+				// find package URI
+				if (!rootObject.TryGetProperty("Packages", out var jsonPackageListElement))
+					throw new ArgumentException("No packages list found.");
+				if (jsonPackageListElement.ValueKind != JsonValueKind.Array)
+					throw new ArgumentException("Package list is not an array.");
+				foreach (var jsonPackageElement in jsonPackageListElement.EnumerateArray())
+				{
+					// check JSON element type
+					if (jsonPackageElement.ValueKind != JsonValueKind.Object)
+						continue;
+
+					// get package URI
+					if (!jsonPackageElement.TryGetProperty("Uri", out jsonValue)
+						|| jsonValue.ValueKind != JsonValueKind.String
+						|| !Uri.TryCreate(jsonValue.GetString().AsNonNull(), UriKind.Absolute, out var uri))
+					{
+						continue;
+					}
+
+					// check OS
+					var hasOsProperty = jsonPackageElement.TryGetProperty("OperatingSystem", out jsonValue);
+					var isOsMatched = (hasOsProperty && jsonValue.ValueKind == JsonValueKind.String && jsonValue.GetString() == osName);
+
+					// check CPU architecture
+					var hasArchProperty = jsonPackageElement.TryGetProperty("Architecture", out jsonValue);
+					var isArchMatched = (hasArchProperty && jsonValue.ValueKind == JsonValueKind.String && jsonValue.GetString() == archName);
+
+					// select package
+					if (!hasOsProperty && !hasArchProperty)
+						genericPackageUri = uri;
+					else if (isOsMatched && isArchMatched)
+					{
+						packageUri = uri;
+						return;
+					}
+				}
+			});
+			if (cancellationToken.IsCancellationRequested)
+				throw new TaskCanceledException();
+
+			// save result
+			this.ApplicationName = appName;
+			this.PageUri = pageUri;
+			this.PackageVersion = packageVersion;
+			this.PackageUri = packageUri ?? genericPackageUri ?? throw new ArgumentException("Package URI not found.");
 		}
 	}
 #endif
--- AutoUpdate/Resolvers/XmlPackageResolver.cs
+++ AutoUpdate/Resolvers/XmlPackageResolver.cs
@@ -29,108 +29,112 @@ namespace CarinaStudio.AutoUpdate.Resolvers
 		{
 			// get XML data
 			using var streamReader = await this.Source.AsNonNull().OpenStreamReaderAsync(StreamAccess.Read, Encoding.UTF8, cancellationToken);
-			var xmlDocument = new XmlDocument().Also(it => it.Load(streamReader));
 
-			// cancellation check
+			// parse package manifest
+			var appName = (string?)null;
+			var packageVersion = (Version?)null;
+			var pageUri = (Uri?)null;
+			var packageUri = (Uri?)null;
+			var genericPackageUri = (Uri?)null;
+			await Task.Run(() =>
+			{
+				// parse as XML document
+				var xmlDocument = new XmlDocument().Also(it => it.Load(streamReader));
+
+				// cancellation check
+				if (cancellationToken.IsCancellationRequested)
+					throw new TaskCanceledException();
+
+				// find root node
+				var rootNode = xmlDocument.Let(it =>
+				{
+					if (!it.HasChildNodes)
+						return null;
+					var node = it.FirstChild;
+					while (node != null)
+					{
+						if (node.NodeType == XmlNodeType.Element && node.Name == "PackageManifest")
+							return node;
+						node = node.NextSibling;
+					}
+					return null;
+				}) ?? throw new XmlException("Node of package manifest not fount.");
+
+				// get name
+				rootNode.Attributes["Name"]?.Let(it => appName = it.Value);
+
+				// get version
+				rootNode.Attributes["Version"]?.Let(it => Version.TryParse(it.Value, out packageVersion));
+
+				// get page URI
+				rootNode.Attributes["PageUri"]?.Let(it => Uri.TryCreate(it.Value, UriKind.Absolute, out pageUri));
+
+				// check platform
+				var osName = Global.Run(() =>
+				{
+					if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+						return nameof(OSPlatform.Windows);
+					if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+						return nameof(OSPlatform.Linux);
+					if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+						return nameof(OSPlatform.OSX);
+					return "";
+				});
+				if (string.IsNullOrEmpty(osName))
+					throw new ArgumentException("Unknown operating system.");
+				var archName = RuntimeInformation.ProcessArchitecture.ToString();
+
+				// find package URI
+				if (!rootNode.HasChildNodes)
+					throw new XmlException("Package not fount.");
+				var packageNode = rootNode.FirstChild;
+				while (packageNode != null)
+				{
+					try
+					{
+						if (packageNode.NodeType == XmlNodeType.Element && packageNode.Name == "Package")
+						{
+							// get URI
+							var uri = packageNode.Attributes["Uri"]?.Let(it =>
+							{
+								Uri.TryCreate(it.Value, UriKind.Absolute, out var uri);
+								return uri;
+							});
+							if (uri == null)
+								continue;
+
+							// check operating system
+							var osAttribute = packageNode.Attributes["OperatingSystem"];
+							var isOsMatched = osAttribute != null && osAttribute.Value == osName;
+
+							// check architecture
+							var archArrtibute = packageNode.Attributes["Architecture"];
+							var isArchMatched = archArrtibute != null && archArrtibute.Value == archName;
+
+							// select package
+							if (osAttribute == null && archArrtibute == null)
+								genericPackageUri = uri;
+							else if (isOsMatched && isArchMatched)
+							{
+								packageUri = uri;
+								break;
+							}
+						}
+					}
+					finally
+					{
+						packageNode = packageNode.NextSibling;
+					}
+				}
+			});
 			if (cancellationToken.IsCancellationRequested)
 				throw new TaskCanceledException();
 
-			// find root node
-			var rootNode = xmlDocument.Let(it =>
-			{
-				if (!it.HasChildNodes)
-					return null;
-				var node = it.FirstChild;
-				while (node != null)
-				{
-					if (node.NodeType == XmlNodeType.Element && node.Name == "PackageManifest")
-						return node;
-					node = node.NextSibling;
-				}
-				return null;
-			}) ?? throw new XmlException("Node of package manifest not fount.");
-
-			// get name
-			rootNode.Attributes["Name"]?.Let(it => this.ApplicationName = it.Value);
-
-			// get version
-			rootNode.Attributes["Version"]?.Let(it =>
-			{
-				if (Version.TryParse(it.Value, out var version))
-					this.PackageVersion = version;
-			});
-
-			// get page URI
-			rootNode.Attributes["PageUri"]?.Let(it =>
-			{
-				if (Uri.TryCreate(it.Value, UriKind.Absolute, out var uri))
-					this.PageUri = uri;
-			});
-
-			// check platform
-			var osName = Global.Run(() =>
-			{
-				if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-					return nameof(OSPlatform.Windows);
-				if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-					return nameof(OSPlatform.Linux);
-				if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-					return nameof(OSPlatform.OSX);
-				return "";
-			});
-			if (string.IsNullOrEmpty(osName))
-				throw new ArgumentException("Unknown operating system.");
-			var archName = RuntimeInformation.ProcessArchitecture.ToString();
-
-			// find package URI
-			if (!rootNode.HasChildNodes)
-				throw new XmlException("Package not fount.");
-			var packageNode = rootNode.FirstChild;
-			var genericPackageUri = (Uri?)null;
-			while (packageNode != null)
-			{
-				try
-				{
-					if (packageNode.NodeType == XmlNodeType.Element && packageNode.Name == "Package")
-					{
-						// get URI
-						var packageUri = packageNode.Attributes["Uri"]?.Let(it =>
-						{
-							Uri.TryCreate(it.Value, UriKind.Absolute, out var uri);
-							return uri;
-						});
-						if (packageUri == null)
-							continue;
-
-						// check operating system
-						var osAttribute = packageNode.Attributes["OperatingSystem"];
-						var isOsMatched = osAttribute != null && osAttribute.Value == osName;
-
-						// check architecture
-						var archArrtibute = packageNode.Attributes["Architecture"];
-						var isArchMatched = archArrtibute != null && archArrtibute.Value == archName;
-
-						// select package
-						if (osAttribute == null && archArrtibute == null)
-							genericPackageUri = packageUri;
-						else if (isOsMatched && isArchMatched)
-						{
-							this.PackageUri = packageUri;
-							break;
-						}
-					}
-				}
-				finally
-				{
-					packageNode = packageNode.NextSibling;
-				}
-			}
-			if (this.PackageUri == null)
-			{
-				if (genericPackageUri == null)
-					throw new XmlException("Package not fount.");
-				this.PackageUri = genericPackageUri;
-			}
+			// save result
+			this.ApplicationName = appName;
+			this.PageUri = pageUri;
+			this.PackageVersion = packageVersion;
+			this.PackageUri = packageUri ?? genericPackageUri ?? throw new ArgumentException("Package URI not found.");
 		}
 	}
 }
