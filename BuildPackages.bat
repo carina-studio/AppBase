1d282e047692a9b1e77e14aadea38a62dfbfc285
Hamster <hamster620@gmail.com>
2021-09-12 14:14:41 +0800

    Add build and package command batch file for Windows.

---
+++ BuildPackages.bat
@@ -0,0 +1,18 @@
+@echo off
+
+dotnet build Application.Avalonia -c Release
+IF %ERRORLEVEL% NEQ 0 ( 
+   exit
+)
+
+dotnet build Tests -c Release
+IF %ERRORLEVEL% NEQ 0 ( 
+   exit
+)
+
+dotnet pack Core -c Release -o Packages
+dotnet pack Configuration -c Release -o Packages
+dotnet pack Application -c Release -o Packages
+dotnet pack Application.Avalonia -c Release -o Packages
+dotnet pack AutoUpdate -c Release -o Packages
+dotnet pack Tests -c Release -o Packages
