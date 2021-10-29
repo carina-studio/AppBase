21caad842f4347a478cc8f4c667c5a4a13a4601a
Hamster <hamster620@gmail.com>
2021-10-29 12:20:38 +0800

    Add ResourceNodeExtensions.

---
+++ Application.Avalonia/Controls/ResourceNodeExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Avalonia.Controls;
+using System;
+
+namespace CarinaStudio.Controls
+{
+    /// <summary>
+    /// Extensions for <see cref="IResourceNode"/>.
+    /// </summary>
+    public static class ResourceNodeExtensions
+    {
+		/// <summary>
+		/// Try getting resource with given type.
+		/// </summary>
+		/// <typeparam name="T">Type of resource.</typeparam>
+		/// <param name="node"><see cref="IResourceNode"/>.</param>
+		/// <param name="key">Resource key.</param>
+		/// <param name="res">Found resource.</param>
+		/// <returns>True if resource got.</returns>
+		public static bool TryGetResource<T>(this IResourceNode node, object key, out T? res) where T : class
+		{
+			if (node.TryGetResource(key, out var rawRes) && rawRes is T)
+			{
+				res = (T)rawRes;
+				return true;
+			}
+			res = default;
+			return false;
+		}
+
+
+		/// <summary>
+		/// Try getting resource with given type.
+		/// </summary>
+		/// <typeparam name="T">Type of resource.</typeparam>
+		/// <param name="node"><see cref="IResourceNode"/>.</param>
+		/// <param name="key">Resource key.</param>
+		/// <param name="res">Found resource.</param>
+		/// <returns>True if resource got.</returns>
+		public static bool TryGetResource<T>(this IResourceNode node, object key, out T? res) where T : struct
+		{
+			if (node.TryGetResource(key, out var rawRes) && rawRes is T)
+			{
+				res = (T)rawRes;
+				return true;
+			}
+			res = default;
+			return false;
+		}
+	}
+}
