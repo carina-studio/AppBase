ba5785197dfe2a58c8d08ce241bbd8fa466f93eb
Hamster <hamster620@gmail.com>
2022-03-16 12:36:34 +0800

    Add ValueTextBox and IntegerTextBox.

---
+++ Avalonia/Controls/IntegerTextBox.cs
@@ -0,0 +1,251 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace CarinaStudio.Controls
+{
+    /// <summary>
+    /// <see cref="TextBox"/> to let user input an integer in decimal.
+    /// </summary>
+    public class IntegerTextBox : ValueTextBox<long>
+    {
+        /// <summary>
+        /// Property of <see cref="AcceptsPositiveSign"/>.
+        /// </summary>
+        public static readonly AvaloniaProperty<bool> AcceptsPositiveSignProperty = AvaloniaProperty.Register<IntegerTextBox, bool>(nameof(AcceptsPositiveSign), false);
+        /// <summary>
+        /// Property of <see cref="Maximum"/>.
+        /// </summary>
+        public static readonly AvaloniaProperty<long> MaximumProperty = AvaloniaProperty.Register<IntegerTextBox, long>(nameof(Maximum), long.MaxValue);
+        /// <summary>
+        /// Property of <see cref="Minimum"/>.
+        /// </summary>
+        public static readonly AvaloniaProperty<long> MinimumProperty = AvaloniaProperty.Register<IntegerTextBox, long>(nameof(Minimum), long.MinValue);
+
+
+        // Constants.
+        const int MaxTextLength = 20; // max value is +9223372036854775807
+
+
+        /// <summary>
+        /// Initialize new <see cref="IntegerTextBox"/> instance.
+        /// </summary>
+        public IntegerTextBox()
+        {
+            this.PseudoClasses.Set(":integerTextBox", true);
+        }
+
+
+        /// <summary>
+        /// Get or set whether positive sign (+) can be accpeted or not.
+        /// </summary>
+        public bool AcceptsPositiveSign
+        {
+            get => this.GetValue<bool>(AcceptsPositiveSignProperty);
+            set => this.SetValue<bool>(AcceptsPositiveSignProperty, value);
+        }
+
+
+        /// <inheritdoc/>
+        protected override long CoerceValue(long value)
+        {
+            if (value < this.Minimum)
+                return Minimum;
+            else if (value > this.Maximum)
+                return this.Maximum;
+            return value;
+        }
+
+
+        /// <inheritdoc/>
+        protected override long DefaultValue 
+        { 
+            get
+            {
+                if (this.Minimum < 0)
+                {
+                    if (this.Maximum > 0)
+                        return 0;
+                    return this.Maximum;
+                }
+                return this.Minimum;
+            }
+        }
+
+
+        /// <summary>
+        /// Get or set maximum value.
+        /// </summary>
+        public long Maximum
+        {
+            get => this.GetValue<long>(MaximumProperty);
+            set
+            {
+                if (value < this.Minimum)
+                    value = this.Minimum;
+                this.SetValue<long>(MaximumProperty, value);
+            }
+        }
+
+
+        /// <summary>
+        /// Get or set minimum value.
+        /// </summary>
+        public long Minimum
+        {
+            get => this.GetValue<long>(MinimumProperty);
+            set
+            {
+                if (value > this.Maximum)
+                    value = this.Maximum;
+                this.SetValue<long>(MinimumProperty, value);
+            }
+        }
+
+
+        /// <inheritdoc/>
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            if (!e.Handled && e.KeyModifiers == 0)
+            {
+                switch (e.Key)
+                {
+                    case Key.Down:
+                        if (this.Validate() && this.Value.HasValue)
+                        {
+                            var value = this.Value.GetValueOrDefault();
+                            if (value > this.Minimum)
+                            {
+                                this.Value = (value - 1);
+                                this.SelectAll();
+                            }
+                        }
+                        e.Handled = true;
+                        break;
+                    case Key.Up:
+                        if (this.Validate() && this.Value.HasValue)
+                        {
+                            var value = this.Value.GetValueOrDefault();
+                            if (value < this.Maximum)
+                            {
+                                this.Value = (value + 1);
+                                this.SelectAll();
+                            }
+                        }
+                        e.Handled = true;
+                        break;
+                }
+            }
+        }
+
+
+        /// <inheritdoc/>
+        protected override void OnPropertyChanged<TProperty>(AvaloniaPropertyChangedEventArgs<TProperty> change)
+        {
+            base.OnPropertyChanged(change);
+            var property = change.Property;
+            if (property == AcceptsPositiveSignProperty)
+            {
+                if (!this.AcceptsPositiveSign)
+                {
+                    var s = this.Text;
+                    if (s != null && s.Length > 0 && s[0] == '+')
+                        this.Text = s.Substring(1);
+                }
+            }
+            else if (property == MaximumProperty || property == MinimumProperty)
+            {
+                this.Validate();
+                if (!this.IsNullValueAllowed)
+                    this.Value = this.CoerceValue(this.Value.GetValueOrDefault());
+            }
+            else if (property == TextProperty)
+            {
+                var s = (change.NewValue.Value as string);
+                if (s != null)
+                {
+                    if (s.Length > MaxTextLength)
+                        s = s.Substring(0, MaxTextLength);
+                    for (var i = 0; i < s.Length; ++i)
+                    {
+                        var c = s[i];
+                        if (c >= '0' && c <= '9')
+                            continue;
+                        if (c == '+')
+                        {
+                            if (this.AcceptsPositiveSign && i == 0)
+                                continue;
+                        }
+                        else if (c == '-')
+                        {
+                            if (i == 0)
+                                continue;
+                        }
+                        var newString = new StringBuilder(s.Substring(0, i));
+                        for (++i; i < s.Length; ++i)
+                        {
+                            c = s[i];
+                            if (c >= '0' && c <= '9')
+                                newString.Append(c);
+                        }
+                        SynchronizationContext.Current?.Post(_ =>
+                        {
+                            if (this.Text == (change.NewValue.Value as string))
+                                this.Text = newString.ToString();
+                        }, null);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        /// <inheritdoc/>
+        protected override void OnTextInput(TextInputEventArgs e)
+        {
+            var s = e.Text;
+            if (s != null && s.Length > 0)
+            {
+                if (this.Text?.Length > MaxTextLength)
+                    e.Text = "";
+                else
+                {
+                    var c = s[0];
+                    if (c < '0' || c > '9')
+                    {
+                        if (c == '+')
+                        {
+                            if (!this.AcceptsPositiveSign || this.Maximum <= 0 || this.SelectionStart != 0)
+                                e.Text = "";
+                        }
+                        else if (c == '-')
+                        {
+                            if (this.Minimum >= 0 || this.SelectionStart != 0)
+                                e.Text = "";
+                        }
+                        else
+                            e.Text = "";
+                    }
+                }
+            }
+            base.OnTextInput(e);
+        }
+
+
+        /// <inheritdoc/>
+        protected override bool TryConvertToValue(string text, out long? value)
+        {
+            if (long.TryParse(text, out var number) && number >= this.Minimum && number <= this.Maximum)
+            {
+                value = number;
+                return true;
+            }
+            value = null;
+            return false;
+        }
+    }
+}
---
+++ Avalonia/Controls/ValueTextBox.cs
@@ -0,0 +1,282 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Styling;
+using CarinaStudio.Threading;
+using System;
+
+namespace CarinaStudio.Controls
+{
+    /// <summary>
+    /// <see cref="TextBox"/> which treat input text as given value with type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">Type of object.</typeparam>
+    public abstract class ValueTextBox<T> : TextBox, IStyleable where T : struct
+	{
+		/// <summary>
+		/// Property of <see cref="IsNullValueAllowed"/>.
+		/// </summary>
+		public static readonly AvaloniaProperty<bool> IsNullValueAllowedProperty = AvaloniaProperty.Register<ValueTextBox<T>, bool>(nameof(IsNullValueAllowed), true);
+		/// <summary>
+		/// Property of <see cref="IsTextValid"/>.
+		/// </summary>
+		public static readonly DirectProperty<ValueTextBox<T>, bool> IsTextValidProperty = AvaloniaProperty.RegisterDirect<ValueTextBox<T>, bool>(nameof(IsTextValid), o => o.isTextValid);
+		/// <summary>
+		/// Property of <see cref="ValidationDelay"/>.
+		/// </summary>
+		public static readonly AvaloniaProperty<int> ValidationDelayProperty = AvaloniaProperty.Register<ValueTextBox<T>, int>(nameof(ValidationDelay), 500, coerce: (_, it) => Math.Max(0, it));
+		/// <summary>
+		/// Property of <see cref="Value"/>.
+		/// </summary>
+		public static readonly AvaloniaProperty<T?> ValueProperty = AvaloniaProperty.Register<ValueTextBox<T>, T?>(nameof(Value), null);
+
+
+		// Fields.
+		bool isTextValid = true;
+		readonly ScheduledAction validateAction;
+
+
+		/// <summary>
+		/// Initialize new <see cref="ValueTextBox{T}"/> instance.
+		/// </summary>
+		protected ValueTextBox()
+		{
+			this.validateAction = new ScheduledAction(() => this.Validate());
+		}
+
+
+		/// <summary>
+		/// Check equality of values.
+		/// </summary>
+		/// <param name="x">First value.</param>
+		/// <param name="y">Second value.</param>
+		/// <returns>True if two values are equalvant.</returns>
+		protected virtual bool CheckValueEquality(T? x, T? y) => x?.Equals(y) ?? y == null;
+
+
+		/// <summary>
+		/// Coerce the set value.
+		/// </summary>
+		/// <param name="value">Set value.</param>
+		/// <returns>Coerced value.</returns>
+		protected virtual T CoerceValue(T value) => value;
+
+
+		/// <summary>
+		/// Convert value to text.
+		/// </summary>
+		/// <param name="value">Value.</param>
+		/// <returns>Converted text.</returns>
+		protected virtual string? ConvertToText(T value) => value.ToString();
+
+
+		/// <summary>
+		/// Default value for <see cref="IsNullValueAllowed"/> is False and <see cref="Avalonia.Controls.TextBox.Text"/> is empty.
+		/// </summary>
+		protected virtual T DefaultValue { get => default(T); }
+
+
+		/// <summary>
+		/// Get or set whether <see cref="Value"/> can be Null or not.
+		/// </summary>
+		public bool IsNullValueAllowed
+		{
+			get => this.GetValue<bool>(IsNullValueAllowedProperty);
+			set => this.SetValue<bool>(IsNullValueAllowedProperty, value);
+		}
+
+
+		/// <summary>
+		/// Get whether input <see cref="TextBox.Text"/> represent a valid value or not.
+		/// </summary>
+		public bool IsTextValid { get => this.isTextValid; }
+
+
+		/// <inheritdoc/>
+		protected override void OnPropertyChanged<TProperty>(AvaloniaPropertyChangedEventArgs<TProperty> change)
+		{
+			base.OnPropertyChanged(change);
+			var property = change.Property;
+			if (property == IsNullValueAllowedProperty)
+			{
+				if (!(bool)((object?)change.NewValue.Value).AsNonNull())
+				{
+					if (!this.validateAction.ExecuteIfScheduled() && this.Value == null)
+						this.ResetToDefaultValue();
+				}
+			}
+			else if (property == TextProperty)
+			{
+				if (string.IsNullOrEmpty(this.Text))
+					this.validateAction.Reschedule();
+				else
+					this.validateAction.Reschedule(this.ValidationDelay);
+			}
+			else if (property == ValidationDelayProperty)
+			{
+				if (this.validateAction.IsScheduled)
+					this.validateAction.Reschedule(this.ValidationDelay);
+			}
+			else if (property == ValueProperty)
+			{
+				var value = (T?)(object?)change.NewValue.Value;
+				if (value == null && !this.IsNullValueAllowed)
+					value = value.GetValueOrDefault();
+				if (value != null)
+				{
+					if (!this.Validate(false, out var currentValue) || !this.CheckValueEquality(currentValue, value))
+					{
+						var fromEmptyString = string.IsNullOrEmpty(this.Text);
+						this.Text = this.ConvertToText(value.Value);
+						this.validateAction.ExecuteIfScheduled();
+						if (this.IsFocused && fromEmptyString && !string.IsNullOrEmpty(this.Text))
+							this.SelectAll();
+					}
+				}
+				else if (this.Text != null)
+					this.Text = "";
+			}
+		}
+
+
+		/// <inheritdoc/>
+		protected override void OnTextInput(TextInputEventArgs e)
+		{
+			if (string.IsNullOrEmpty(this.Text))
+			{
+				var text = e.Text;
+				if (text != null && text.Length > 0 && char.IsWhiteSpace(text[0]))
+					e.Handled = true;
+			}
+			base.OnTextInput(e);
+		}
+
+
+		// Reset to default value.
+		void ResetToDefaultValue()
+		{
+			var value = this.DefaultValue;
+			this.SetValue<T?>(ValueProperty, value);
+			this.Text = this.ConvertToText(value);
+			this.SetAndRaise<bool>(IsTextValidProperty, ref this.isTextValid, true);
+			this.PseudoClasses.Set(":invalidValueTextBoxText", false);
+			this.validateAction.Cancel();
+			if (this.IsFocused)
+				this.SelectAll();
+		}
+
+		/// <summary>
+		/// Try converting text to value.
+		/// </summary>
+		/// <param name="text">Text.</param>
+		/// <param name="value">Converted value.</param>
+		/// <returns>True if conversion succeeded.</returns>
+		protected abstract bool TryConvertToValue(string text, out T? value);
+
+
+		/// <summary>
+		/// Validate input <see cref="TextBox.Text"/> and generate corresponding value.
+		/// </summary>
+		/// <returns>True if input <see cref="TextBox.Text"/> generates a valid value.</returns>
+		public bool Validate() =>
+			this.Validate(true, out var value);
+
+
+		// Validate text.
+		bool Validate(bool updateValueAndText, out T? value)
+		{
+			// check state
+			this.VerifyAccess();
+
+			// cancel scheduled validation
+			if (updateValueAndText)
+				this.validateAction.Cancel();
+
+			// trim spaces
+			var text = this.Text ?? "";
+			var trimmedText = text.Trim();
+			if (text != trimmedText)
+			{
+				text = trimmedText;
+				if (updateValueAndText)
+				{
+					this.Text = trimmedText;
+					this.validateAction.Cancel();
+				}
+			}
+
+			// clear value
+			if (text.Length == 0)
+			{
+				if (this.IsNullValueAllowed)
+				{
+					value = null;
+					if (updateValueAndText)
+					{
+						this.SetValue<T?>(ValueProperty, null);
+						this.SetAndRaise<bool>(IsTextValidProperty, ref this.isTextValid, true);
+						this.PseudoClasses.Set(":invalidValueTextBoxText", false);
+					}
+				}
+				else
+				{
+					value = this.DefaultValue;
+					if (updateValueAndText)
+						this.ResetToDefaultValue();
+				}
+				return true;
+			}
+
+			// try convert to object
+			if (!this.TryConvertToValue(text, out value) || value == null)
+			{
+				if (updateValueAndText)
+				{
+					this.SetAndRaise<bool>(IsTextValidProperty, ref this.isTextValid, false);
+					this.PseudoClasses.Set(":invalidValueTextBoxText", true);
+				}
+				return false;
+			}
+
+			// complete
+			if (updateValueAndText)
+			{
+				if (!this.CheckValueEquality(value, this.Value))
+					this.SetValue<T?>(ValueProperty, value);
+				this.SetAndRaise<bool>(IsTextValidProperty, ref this.isTextValid, true);
+				this.PseudoClasses.Set(":invalidValueTextBoxText", false);
+			}
+			return true;
+		}
+
+
+		/// <summary>
+		/// Get or set the delay of validating text after user typing in milliseconds.
+		/// </summary>
+		public int ValidationDelay
+		{
+			get => this.GetValue<int>(ValidationDelayProperty);
+			set => this.SetValue<int>(ValidationDelayProperty, value);
+		}
+
+
+		/// <summary>
+		/// Get or set value.
+		/// </summary>
+		public T? Value
+		{
+			get => this.GetValue<T?>(ValueProperty);
+			set
+			{
+				this.validateAction.ExecuteIfScheduled();
+				if (value == null && !this.IsNullValueAllowed)
+					value = value.GetValueOrDefault();
+				this.SetValue<T?>(ValueProperty, value != null ? this.CoerceValue(value.GetValueOrDefault()) : (T?)null);
+			}
+		}
+
+
+		// Interface implementations.
+		Type IStyleable.StyleKey => typeof(TextBox);
+	}
+}
