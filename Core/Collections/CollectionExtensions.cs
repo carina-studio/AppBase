6412201850465cbb95917795abca55946ab992d3
Hamster <hamster620@gmail.com>
2022-05-12 09:32:18 +0800

    Add ICollection<T>.AddAll() and ICollection<T>.RemoveAll() extension methods.

--- Core/Collections/CollectionExtensions.cs
+++ Core/Collections/CollectionExtensions.cs
@@ -10,6 +10,28 @@ namespace CarinaStudio.Collections
 	public static class CollectionExtensions
 	{
 		/// <summary>
+		/// Add all given elements to collection.
+		/// </summary>
+		/// <param name="collection">Collection.</param>
+		/// <param name="elements">Elements to add.</param>
+		/// <typeparam name="T">Type of element of collection.</typeparam>
+		public static void AddAll<T>(this ICollection<T> collection, IEnumerable<T> elements)
+		{
+			if (collection is List<T> list)
+				list.AddRange(elements);
+			else if (collection is ObservableList<T> observableList)
+				observableList.AddRange(elements);
+			else if (collection is SortedObservableList<T> sortedList)
+				sortedList.AddAll(elements);
+			else
+			{
+				foreach (var element in elements)
+					collection.Add(element);
+			}
+		}
+
+
+		/// <summary>
 		/// Check whether given collection is empty or not.
 		/// </summary>
 		/// <typeparam name="T">Type of element of collection.</typeparam>
@@ -37,5 +59,52 @@ namespace CarinaStudio.Collections
 		/// <returns>True if collection is null or empty.</returns>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static bool IsNullOrEmpty<T>(this ICollection<T>? collection) => collection == null || collection.Count <= 0;
+
+
+		/// <summary>
+		/// Remove elements which match the given condition from collection.
+		/// </summary>
+		/// <param name="collection">Collection.</param>
+		/// <param name="predicate">Function to check condition.</param>
+		/// <typeparam name="T">Type of element of collection.</typeparam>
+		/// <returns>Number of removed elements.</returns>
+		public static int RemoveAll<T>(this ICollection<T> collection, Predicate<T> predicate)
+		{
+			if (collection is List<T> list)
+				return list.RemoveAll(predicate);
+			if (collection is ObservableList<T> observableList)
+				return observableList.RemoveAll(predicate);
+			if (collection is SortedObservableList<T> sortedList)
+				return sortedList.RemoveAll(predicate);
+			if (collection is IList<T> listInterface)
+			{
+				var count = 0;
+				for (var i = listInterface.Count - 1; i >= 0; --i)
+				{
+					if (predicate(listInterface[i]))
+					{
+						listInterface.RemoveAt(i);
+						++count;
+					}
+				}
+				return count;
+			}
+			var elementsToRemove = (List<T>?)null;
+			foreach (var element in collection)
+			{
+				if (predicate(element))
+				{
+					elementsToRemove ??= new List<T>();
+					elementsToRemove.Add(element);
+				}
+			}
+			if (elementsToRemove != null)
+			{
+				foreach (var element in elementsToRemove)
+					collection.Remove(element);
+				return elementsToRemove.Count;
+			}
+			return 0;
+		}
 	}
 }
