5140c408275d5826025ac75a6e33f6e2dce91f86
Hamster <hamster620@gmail.com>
2022-09-24 21:03:12 +0800

    Implement NSApplication (1/n).

--- MacOS/NativeLibraryNames.cs
+++ MacOS/NativeLibraryNames.cs
@@ -6,6 +6,10 @@ namespace CarinaStudio.MacOS
     public static class NativeLibraryNames
     {
         /// <summary>
+        /// AppKit.
+        /// </summary>
+        public const string AppKit = "/System/Library/Frameworks/AppKit.framework/AppKit";
+        /// <summary>
         /// Core Foundation.
         /// </summary>
         public const string CoreFoundation = "/System/Library/Frameworks/ApplicationServices.framework/Frameworks/CoreFoundation.framework/CoreFoundation";
@@ -21,5 +25,9 @@ namespace CarinaStudio.MacOS
         /// Objective-C Runtime.
         /// </summary>
         public const string ObjectiveC = "/usr/lib/libobjc.dylib";
+        /// <summary>
+        /// System.
+        /// </summary>
+        public const string System = "/usr/lib/libSystem.dylib";
     }
 }
