ff90bac219bcaf414d9e0633b06ccb819af1fdc0
Hamster <hamster620@gmail.com>
2022-09-29 08:51:54 +0800

    [1] Support sending message to super class. [2] Support passing enum value and GCHandle to/from Objective-C runtime.

--- Application.Avalonia.Tests/App.axaml.cs
+++ Application.Avalonia.Tests/App.axaml.cs
@@ -34,6 +34,8 @@ namespace CarinaStudio
             base.OnFrameworkInitializationCompleted();
             (this.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.Let(it =>
             {
+                if (Platform.IsMacOS)
+                    it.ShutdownMode = Avalonia.Controls.ShutdownMode.OnExplicitShutdown;
                 it.MainWindow = new MainWindow();
             });
         }
