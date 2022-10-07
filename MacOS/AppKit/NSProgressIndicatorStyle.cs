69bfeeec9295dce6ccc60925f41d4e6cda56f828
Hamster <hamster620@gmail.com>
2022-10-07 21:21:12 +0800

    [1] Apply reference counting on NSObject. [2] Implement NSProgressIndicator.

---
+++ MacOS/AppKit/NSProgressIndicatorStyle.cs
@@ -0,0 +1,12 @@
+namespace CarinaStudio.MacOS.AppKit;
+
+#pragma warning disable CS1591
+
+/// <summary>
+/// NSProgressIndicatorStyle.
+/// </summary>
+public enum NSProgressIndicatorStyle : uint
+{
+    Bar = 0,
+    Spinning = 1,
+}
