a2190b25ff1190354c7561d1b854c11d15a4a398
Hamster <hamster620@gmail.com>
2023-02-17 14:13:28 +0800

    Add CachedObservableValue<T> and its tests.

---
+++ Core.Tests/CachedObservableValueTests.cs
@@ -0,0 +1,96 @@
+using CarinaStudio.Threading;
+using NUnit.Framework;
+using System;
+using System.Threading;
+
+namespace CarinaStudio
+{
+    /// <summary>
+    /// Tests of <see cref="CachedObservableValue{T}"/>.
+    /// </summary>
+    [TestFixture]
+    public class CachedObservableValueTests
+    {
+        /// <summary>
+        /// Test for updating cached value by given function.
+        /// </summary>
+        [Test]
+        public void UpdatingByFunctionTest()
+        {
+            // prepare
+            var random = new Random();
+            var latestUpdatedValue = 0;
+            var updateFunc = new Func<int>(() => 
+            {
+                latestUpdatedValue = random.Next();
+                return latestUpdatedValue;
+            });
+
+            // create
+            var cachedValue = new CachedObservableValue<int>(updateFunc);
+            Assert.AreEqual(latestUpdatedValue, cachedValue.Value);
+
+            // update value
+            latestUpdatedValue = -1;
+            cachedValue.Invalidate();
+            Assert.GreaterOrEqual(latestUpdatedValue, 0);
+            Assert.AreEqual(latestUpdatedValue, cachedValue.Value);
+        }
+
+
+        /// <summary>
+        /// Test for updating cached value by another observable value.
+        /// </summary>
+        [Test]
+        public void UpdatingByObservableTest()
+        {
+            // prepare
+            var random = new Random();
+            var sourceValue = new MutableObservableInt32(random.Next());
+            using var syncContext = new SingleThreadSynchronizationContext();
+
+            // update value in current thread
+            var cachedValueRef = default(WeakReference<CachedObservableValue<int>>);
+            var testAction = new Action(() => 
+            {
+                // create
+                var cachedValue = new CachedObservableValue<int>(sourceValue);
+                Assert.AreEqual(sourceValue.Value, cachedValue.Value);
+
+                // update value
+                for (var i = 0; i < 10; ++i)
+                {
+                    sourceValue.Update(random.Next());
+                    Assert.AreEqual(sourceValue.Value, cachedValue.Value);
+                }
+                cachedValueRef = new(cachedValue);
+            });
+            testAction();
+            for (var i = 9; i >= 0; --i)
+            {
+                GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+                GC.WaitForPendingFinalizers();
+                if (cachedValueRef?.TryGetTarget(out var _) != true)
+                    break;
+                if (i == 0)
+                    throw new AssertionException("Unable to finalize cached value.");
+                Thread.Sleep(500);
+            }
+            Assert.IsFalse(sourceValue.HasObservers);
+
+            // create in specific thread
+            syncContext.Send(testAction);
+            for (var i = 9; i >= 0; --i)
+            {
+                GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+                GC.WaitForPendingFinalizers();
+                if (cachedValueRef?.TryGetTarget(out var _) != true)
+                    break;
+                if (i == 0)
+                    throw new AssertionException("Unable to finalize cached value.");
+                Thread.Sleep(500);
+            }
+            syncContext.Send(() => Assert.IsFalse(sourceValue.HasObservers));
+        }
+    }
+}
---
+++ Core/CachedObservableValue.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Threading;
+
+namespace CarinaStudio
+{
+    /// <summary>
+    /// <see cref="ObservableValue{T}"/> which caches a local value from source.
+    /// </summary>
+    /// <typeparam name="T">Type of value.</typeparam>
+    public class CachedObservableValue<T> : ObservableValue<T>
+    {
+        // Observer to source value.
+        class WeakObserver : IObserver<T>
+        {
+            // Fields.
+            readonly WeakReference<CachedObservableValue<T>> owner;
+
+            // Constructor.
+            public WeakObserver(CachedObservableValue<T> owner) =>
+                this.owner = new(owner);
+            
+            /// <inheritdoc/>
+            public void OnCompleted()
+            { }
+
+            /// <inheritdoc/>
+            public void OnError(Exception error)
+            { }
+
+            /// <inheritdoc/>
+            public void OnNext(T value)
+            {
+                if (this.owner.TryGetTarget(out var owner))
+                    owner.Value = value;
+            }
+        }
+
+
+        // Fields.
+        IDisposable? sourceObserverToken;
+        SynchronizationContext? sourceSyncContext;
+        readonly Func<T>? updateValueFunc;
+
+
+        /// <summary>
+        /// Initialize new <see cref="CachedObservableValue{T}"/> instance.
+        /// </summary>
+        /// <param name="updateValueFunc">Function to get value from source.</param>
+        public CachedObservableValue(Func<T> updateValueFunc) : base(updateValueFunc())
+        {
+            this.updateValueFunc = updateValueFunc;
+            GC.SuppressFinalize(this);
+        }
+        
+
+        /// <summary>
+        /// Initialize new <see cref="CachedObservableValue{T}"/> instance.
+        /// </summary>
+        /// <param name="source">Source value.</param>
+        /// <remarks>The source will be observed with weak observer internally. The observer will be unsubscribed in the thread which creates the instance if possible.</remarks>
+        public CachedObservableValue(IObservable<T> source)
+        {
+            this.sourceObserverToken = source.Subscribe(new WeakObserver(this));
+            this.sourceSyncContext = SynchronizationContext.Current;
+        }
+        
+
+        /// <summary>
+        /// Finalizer.
+        /// </summary>
+        ~CachedObservableValue()
+        {
+            if (this.sourceSyncContext != null)
+            {
+                this.sourceSyncContext.Post(_ =>
+                    this.sourceObserverToken = this.sourceObserverToken.DisposeAndReturnNull(), null);
+                this.sourceSyncContext = null;
+            }
+            else
+                this.sourceObserverToken = this.sourceObserverToken.DisposeAndReturnNull();
+        }
+            
+
+        /// <summary>
+        /// Invalidate cached value and update immediately.
+        /// </summary>
+        public void Invalidate()
+        {
+            if (this.updateValueFunc != null)
+                this.Value = this.updateValueFunc();
+        }
+    }
+}
