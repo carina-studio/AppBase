43e8799acab20cb94cc85b8793995a3e8def66f9
Hamster <hamster620@gmail.com>
2025-10-02 21:14:39 +0800

    Add extension methods for ReaderWriterLock/ReaderWriterLockSlim and related unit test.

---
+++ Core.Tests/Threading/ReaderWriterLockExtensionsTests.cs
@@ -0,0 +1,197 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading;
+
+namespace CarinaStudio.Threading;
+
+/// <summary>
+/// Tests for <see cref="ReaderWriterLockExtensions"/>.
+/// </summary>
+[TestFixture]
+public class ReaderWriterLockExtensionsTests
+{
+    /// <summary>
+    /// Test for entering read/write scope.
+    /// </summary>
+    [Test]
+    public void ReadWriteScopeTest()
+    {
+        // normal reader/writer lock (ReaderWriterLockSlim)
+        var value = 1;
+        using var rwLockSlim = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+        using var event1 = new ManualResetEventSlim();
+        using var event2 = new ManualResetEventSlim();
+        ThreadPool.QueueUserWorkItem(_ =>
+        {
+            using var scope = rwLockSlim.EnterWriteScope();
+            event1.Set();
+            Thread.Sleep(1000);
+            Interlocked.Increment(ref value);
+        });
+        Assert.That(event1.Wait(5000));
+        event1.Reset();
+        using (rwLockSlim.EnterReadScope())
+        {
+            using var innerScope = rwLockSlim.EnterReadScope(1);
+            Assert.That(value == 2);
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                Assert.That(event2.Wait(5000));
+                using var scope = rwLockSlim.EnterWriteScope();
+                Interlocked.Increment(ref value);
+                event1.Set();
+            });
+            event2.Set();
+            Thread.Sleep(1000);
+            Assert.That(value == 2);
+        }
+        Assert.That(event1.Wait(5000));
+        Assert.That(value == 3);
+        
+        // reader lock timeout (ReaderWriterLockSlim)
+        event1.Reset();
+        event2.Reset();
+        ThreadPool.QueueUserWorkItem(_ =>
+        {
+            using var scope = rwLockSlim.EnterWriteScope();
+            event1.Set();
+            Thread.Sleep(3000);
+            event1.Set();
+        });
+        Assert.That(event1.Wait(5000));
+        event1.Reset();
+        try
+        {
+            using var _ = rwLockSlim.EnterReadScope(1000);
+            Assert.Fail();
+        }
+        catch (TimeoutException)
+        { }
+        Assert.That(event1.Wait(5000));
+        
+        // writer lock timeout (ReaderWriterLockSlim)
+        event1.Reset();
+        event2.Reset();
+        ThreadPool.QueueUserWorkItem(_ =>
+        {
+            using var scope = rwLockSlim.EnterReadScope();
+            event1.Set();
+            Thread.Sleep(3000);
+            event1.Set();
+        });
+        Assert.That(event1.Wait(5000));
+        event1.Reset();
+        try
+        {
+            using var _ = rwLockSlim.EnterWriteScope(1000);
+            Assert.Fail();
+        }
+        catch (TimeoutException)
+        { }
+        Assert.That(event1.Wait(5000));
+        
+        // upgradeable reader lock
+        value = 1;
+        event1.Reset();
+        event2.Reset();
+        ThreadPool.QueueUserWorkItem(_ =>
+        {
+            using (rwLockSlim.EnterWriteScope())
+            {
+                event1.Set();
+                Thread.Sleep(1000);
+                Interlocked.Increment(ref value);
+            }
+            Assert.That(event2.Wait(5000));
+            using (rwLockSlim.EnterReadScope())
+            {
+                Assert.That(value == 3);
+            }
+        });
+        Assert.That(event1.Wait(5000));
+        using (rwLockSlim.EnterUpgradeableReadScope())
+        {
+            Assert.That(value == 2);
+            using (rwLockSlim.EnterWriteScope())
+            {
+                event2.Set();
+                Thread.Sleep(1000);
+                Interlocked.Increment(ref value);
+            }
+        }
+        
+        // normal reader/writer lock (ReaderWriterLock)
+        var rwLock = new ReaderWriterLock();
+        value = 1;
+        event1.Reset();
+        event2.Reset();
+        ThreadPool.QueueUserWorkItem(_ =>
+        {
+            using var scope = rwLock.EnterWriteScope();
+            event1.Set();
+            Thread.Sleep(1000);
+            Interlocked.Increment(ref value);
+        });
+        Assert.That(event1.Wait(5000));
+        event1.Reset();
+        using (rwLock.EnterReadScope())
+        {
+            using var innerScope = rwLock.EnterReadScope(1);
+            Assert.That(value == 2);
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                Assert.That(event2.Wait(5000));
+                using var scope = rwLock.EnterWriteScope();
+                Interlocked.Increment(ref value);
+                event1.Set();
+            });
+            event2.Set();
+            Thread.Sleep(1000);
+            Assert.That(value == 2);
+        }
+        Assert.That(event1.Wait(5000));
+        Assert.That(value == 3);
+        
+        // reader lock timeout (ReaderWriterLock)
+        event1.Reset();
+        event2.Reset();
+        ThreadPool.QueueUserWorkItem(_ =>
+        {
+            using var scope = rwLock.EnterWriteScope();
+            event1.Set();
+            Thread.Sleep(3000);
+            event1.Set();
+        });
+        Assert.That(event1.Wait(5000));
+        event1.Reset();
+        try
+        {
+            using var _ = rwLock.EnterReadScope(1000);
+            Assert.Fail();
+        }
+        catch (TimeoutException)
+        { }
+        Assert.That(event1.Wait(5000));
+        
+        // writer lock timeout (ReaderWriterLock)
+        event1.Reset();
+        event2.Reset();
+        ThreadPool.QueueUserWorkItem(_ =>
+        {
+            using var scope = rwLock.EnterReadScope();
+            event1.Set();
+            Thread.Sleep(3000);
+            event1.Set();
+        });
+        Assert.That(event1.Wait(5000));
+        event1.Reset();
+        try
+        {
+            using var _ = rwLock.EnterWriteScope(1000);
+            Assert.Fail();
+        }
+        catch (TimeoutException)
+        { }
+        Assert.That(event1.Wait(5000));
+    }
+}
---
+++ Core/Threading/ReaderWriterLockExtensions.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Threading;
+
+namespace CarinaStudio.Threading;
+
+/// <summary>
+/// Represents a scope with reader lock of <see cref="ReaderWriterLock"/> acquired.
+/// </summary>
+public ref struct ReaderLockScope: IDisposable
+{
+    // Fields.
+    bool isDisposed;
+    readonly ReaderWriterLock @lock;
+
+    // Constructor.
+    internal ReaderLockScope(ReaderWriterLock @lock, int timeout)
+    {
+        this.@lock = @lock;
+        try
+        {
+            @lock.AcquireReaderLock(timeout);
+        }
+        catch (Exception ex)
+        {
+            throw new TimeoutException("Timeout before reader lock acquired.", ex);
+        }
+    }
+    
+    /// <interitdoc/>
+    public void Dispose()
+    {
+        if (!this.isDisposed)
+        {
+            this.isDisposed = true;
+            this.@lock.ReleaseReaderLock();
+        }
+    }
+}
+
+
+/// <summary>
+/// Represents a scope with reader lock of <see cref="ReaderWriterLockSlim"/> acquired.
+/// </summary>
+public ref struct ReaderLockScopeSlim: IDisposable
+{
+    // Fields.
+    bool isDisposed;
+    readonly ReaderWriterLockSlim @lock;
+
+    // Constructor.
+    internal ReaderLockScopeSlim(ReaderWriterLockSlim @lock, int timeout)
+    {
+        this.@lock = @lock;
+        if (!@lock.TryEnterReadLock(timeout))
+            throw new TimeoutException("Timeout before reader lock acquired.");
+    }
+    
+    /// <interitdoc/>
+    public void Dispose()
+    {
+        if (!this.isDisposed)
+        {
+            this.isDisposed = true;
+            this.@lock.ExitReadLock();
+        }
+    }
+}
+
+
+/// <summary>
+/// Represents a scope with upgradeable reader lock of <see cref="ReaderWriterLockSlim"/> acquired.
+/// </summary>
+public ref struct UpgradeableReaderLockScopeSlim: IDisposable
+{
+    // Fields.
+    bool isDisposed;
+    readonly ReaderWriterLockSlim @lock;
+
+    // Constructor.
+    internal UpgradeableReaderLockScopeSlim(ReaderWriterLockSlim @lock, int timeout)
+    {
+        this.@lock = @lock;
+        if (!@lock.TryEnterUpgradeableReadLock(timeout))
+            throw new TimeoutException("Timeout before upgradeable reader lock acquired.");
+    }
+    
+    /// <interitdoc/>
+    public void Dispose()
+    {
+        if (!this.isDisposed)
+        {
+            this.isDisposed = true;
+            this.@lock.ExitUpgradeableReadLock();
+        }
+    }
+}
+
+
+/// <summary>
+/// Represents a scope with writer lock of <see cref="ReaderWriterLock"/> acquired.
+/// </summary>
+public ref struct WriterLockScope: IDisposable
+{
+    // Fields.
+    bool isDisposed;
+    readonly ReaderWriterLock @lock;
+
+    // Constructor.
+    internal WriterLockScope(ReaderWriterLock @lock, int timeout)
+    {
+        this.@lock = @lock;
+        try
+        {
+            @lock.AcquireWriterLock(timeout);
+        }
+        catch (ApplicationException ex)
+        {
+            throw new TimeoutException("Timeout before writer lock acquired.", ex);
+        }
+    }
+    
+    /// <interitdoc/>
+    public void Dispose()
+    {
+        if (!this.isDisposed)
+        {
+            this.isDisposed = true;
+            this.@lock.ReleaseWriterLock();
+        }
+    }
+}
+
+
+/// <summary>
+/// Represents a scope with writer lock of <see cref="ReaderWriterLockSlim"/> acquired.
+/// </summary>
+public ref struct WriterLockScopeSlim: IDisposable
+{
+    // Fields.
+    bool isDisposed;
+    readonly ReaderWriterLockSlim @lock;
+
+    // Constructor.
+    internal WriterLockScopeSlim(ReaderWriterLockSlim @lock, int timeout)
+    {
+        this.@lock = @lock;
+        if (!@lock.TryEnterWriteLock(timeout))
+            throw new TimeoutException("Timeout before writer lock acquired.");
+    }
+    
+    /// <interitdoc/>
+    public void Dispose()
+    {
+        if (!this.isDisposed)
+        {
+            this.isDisposed = true;
+            this.@lock.ExitWriteLock();
+        }
+    }
+}
+
+
+/// <summary>
+/// Extension methods for <see cref="ReaderWriterLock"/> and <see cref="ReaderWriterLockSlim"/>.
+/// </summary>
+public static class ReaderWriterLockExtensions
+{
+    // Convert timeout in TimeSpan into milliseconds.
+    static int ConvertTimeoutToMilliseconds(TimeSpan timeout)
+    {
+        var totalMills = timeout.TotalMilliseconds;
+        if (totalMills < -1 || totalMills > int.MaxValue)
+            throw new ArgumentException("Invalid timeout.");
+        return (int)totalMills;
+    }
+    
+    
+    /// <summary>
+    /// Enter the scope with reader lock acquired.
+    /// </summary>
+    /// <param name="lock"><see cref="ReaderWriterLock"/>.</param>
+    /// <param name="timeout">Timeout before reader lock acquired.</param>
+    /// <returns>Scope with reader lock acquired.</returns>
+    public static ReaderLockScope EnterReadScope(this ReaderWriterLock @lock, int timeout = Timeout.Infinite) => new(@lock, timeout);
+    
+    
+    /// <summary>
+    /// Enter the scope with reader lock acquired.
+    /// </summary>
+    /// <param name="lock"><see cref="ReaderWriterLock"/>.</param>
+    /// <param name="timeout">Timeout before reader lock acquired.</param>
+    /// <returns>Scope with reader lock acquired.</returns>
+    public static ReaderLockScope EnterReadScope(this ReaderWriterLock @lock, TimeSpan timeout) => new(@lock, ConvertTimeoutToMilliseconds(timeout));
+    
+    
+    /// <summary>
+    /// Enter the scope with reader lock acquired.
+    /// </summary>
+    /// <param name="lock"><see cref="ReaderWriterLockSlim"/>.</param>
+    /// <param name="timeout">Timeout before reader lock acquired.</param>
+    /// <returns>Scope with reader lock acquired.</returns>
+    public static ReaderLockScopeSlim EnterReadScope(this ReaderWriterLockSlim @lock, int timeout = Timeout.Infinite) => new(@lock, timeout);
+    
+    
+    /// <summary>
+    /// Enter the scope with reader lock acquired.
+    /// </summary>
+    /// <param name="lock"><see cref="ReaderWriterLockSlim"/>.</param>
+    /// <param name="timeout">Timeout before reader lock acquired.</param>
+    /// <returns>Scope with reader lock acquired.</returns>
+    public static ReaderLockScopeSlim EnterReadScope(this ReaderWriterLockSlim @lock, TimeSpan timeout) => new(@lock, ConvertTimeoutToMilliseconds(timeout));
+    
+    
+    /// <summary>
+    /// Enter the scope with upgradeable reader lock acquired.
+    /// </summary>
+    /// <param name="lock"><see cref="ReaderWriterLockSlim"/>.</param>
+    /// <param name="timeout">Timeout before upgradeable reader lock acquired.</param>
+    /// <returns>Scope with upgradeable reader lock acquired.</returns>
+    public static UpgradeableReaderLockScopeSlim EnterUpgradeableReadScope(this ReaderWriterLockSlim @lock, int timeout = Timeout.Infinite) => new(@lock, timeout);
+    
+    
+    /// <summary>
+    /// Enter the scope with upgradeable reader lock acquired.
+    /// </summary>
+    /// <param name="lock"><see cref="ReaderWriterLockSlim"/>.</param>
+    /// <param name="timeout">Timeout before upgradeable reader lock acquired.</param>
+    /// <returns>Scope with upgradeable reader lock acquired.</returns>
+    public static UpgradeableReaderLockScopeSlim EnterUpgradeableReadScope(this ReaderWriterLockSlim @lock, TimeSpan timeout) => new(@lock, ConvertTimeoutToMilliseconds(timeout));
+    
+    
+    /// <summary>
+    /// Enter the scope with writer lock acquired.
+    /// </summary>
+    /// <param name="lock"><see cref="ReaderWriterLock"/>.</param>
+    /// <param name="timeout">Timeout before writer lock acquired.</param>
+    /// <returns>Scope with writer lock acquired.</returns>
+    public static WriterLockScope EnterWriteScope(this ReaderWriterLock @lock, int timeout = Timeout.Infinite) => new(@lock, timeout);
+    
+    
+    /// <summary>
+    /// Enter the scope with writer lock acquired.
+    /// </summary>
+    /// <param name="lock"><see cref="ReaderWriterLock"/>.</param>
+    /// <param name="timeout">Timeout before writer lock acquired.</param>
+    /// <returns>Scope with writer lock acquired.</returns>
+    public static WriterLockScope EnterWriteScope(this ReaderWriterLock @lock, TimeSpan timeout) => new(@lock, ConvertTimeoutToMilliseconds(timeout));
+    
+    
+    /// <summary>
+    /// Enter the scope with writer lock acquired.
+    /// </summary>
+    /// <param name="lock"><see cref="ReaderWriterLockSlim"/>.</param>
+    /// <param name="timeout">Timeout before writer lock acquired.</param>
+    /// <returns>Scope with writer lock acquired.</returns>
+    public static WriterLockScopeSlim EnterWriteScope(this ReaderWriterLockSlim @lock, int timeout = Timeout.Infinite) => new(@lock, timeout);
+    
+    
+    /// <summary>
+    /// Enter the scope with writer lock acquired.
+    /// </summary>
+    /// <param name="lock"><see cref="ReaderWriterLockSlim"/>.</param>
+    /// <param name="timeout">Timeout before writer lock acquired.</param>
+    /// <returns>Scope with writer lock acquired.</returns>
+    public static WriterLockScopeSlim EnterWriteScope(this ReaderWriterLockSlim @lock, TimeSpan timeout) => new(@lock, ConvertTimeoutToMilliseconds(timeout));
+}
