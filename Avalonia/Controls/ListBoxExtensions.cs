cce6341ef5b6ca82db24191b9b4d5350b75fe750
Carina <hamster620@gmail.com>
2026-01-19 11:33:45 +0800

    Add ListBox.TryMoveItem<T>() extension method.

--- Avalonia/Controls/ListBoxExtensions.cs
+++ Avalonia/Controls/ListBoxExtensions.cs
@@ -1,45 +1,115 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia.Collections;
+using Avalonia.Controls;
 using Avalonia.Controls.Presenters;
+using Avalonia.Threading;
 using Avalonia.VisualTree;
+using CarinaStudio.Collections;
+using System;
+using System.Collections;
+using System.Collections.ObjectModel;
 
-namespace CarinaStudio.Controls
+namespace CarinaStudio.Controls;
+
+/// <summary>
+/// Extensions for <see cref="ListBox"/>.
+/// </summary>
+public static class ListBoxExtensions
 {
 	/// <summary>
-	/// Extensions for <see cref="ListBox"/>.
+	/// Try finding <see cref="ListBoxItem"/> of given item in <see cref="ListBox"/>.
 	/// </summary>
-	public static class ListBoxExtensions
+	/// <param name="listBox"><see cref="ListBox"/>.</param>
+	/// <param name="item">Item.</param>
+	/// <param name="listBoxItem">Found <see cref="ListBoxItem"/>.</param>
+	/// <returns>True if <see cref="ListBoxItem"/> found.</returns>
+	public static bool TryFindListBoxItem(this ListBox listBox, object item, out ListBoxItem? listBoxItem)
 	{
-		/// <summary>
-		/// Try finding <see cref="ListBoxItem"/> of given item in <see cref="ListBox"/>.
-		/// </summary>
-		/// <param name="listBox"><see cref="ListBox"/>.</param>
-		/// <param name="item">Item.</param>
-		/// <param name="listBoxItem">Found <see cref="ListBoxItem"/>.</param>
-		/// <returns>True if <see cref="ListBoxItem"/> found.</returns>
-		public static bool TryFindListBoxItem(this ListBox listBox, object item, out ListBoxItem? listBoxItem)
+		var presenter = listBox.FindDescendantOfType<ItemsPresenter>();
+		if (presenter == null)
 		{
-			var presenter = listBox.FindDescendantOfType<ItemsPresenter>();
-			if (presenter == null)
-			{
-				listBoxItem = null;
-				return false;
-			}
-			foreach (var child in presenter.GetVisualChildren())
-			{
-				if (child is Panel panel)
-				{
-					foreach (var panelChild in panel.Children)
-					{
-						if (panelChild is ListBoxItem candidate && candidate.DataContext?.Equals(item) == true)
-						{
-							listBoxItem = candidate;
-							return true;
-						}
-					}
-				}
-			}
 			listBoxItem = null;
 			return false;
 		}
+		foreach (var child in presenter.GetVisualChildren())
+		{
+			if (child is Panel panel)
+			{
+				foreach (var panelChild in panel.Children)
+				{
+					if (panelChild is ListBoxItem candidate && candidate.DataContext?.Equals(item) == true)
+					{
+						listBoxItem = candidate;
+						return true;
+					}
+				}
+			}
+		}
+		listBoxItem = null;
+		return false;
+	}
+	
+	
+	/// <summary>
+	/// Try moving specific item shown in the <see cref="ListBox"/>.
+	/// </summary>
+	/// <param name="listBox"><see cref="ListBox"/>.</param>
+	/// <param name="index">Index of item to be moved.</param>
+	/// <param name="newIndex">Index of new position of item.</param>
+	/// <param name="moveItemFunc">Function to perform moving item, or Null to use default way to move item.</param>
+	/// <param name="scrollIntoView">True to scroll the item into view after moving.</param>
+	/// <typeparam name="T">Type of element in the collection shown by the <see cref="ListBox"/>.</typeparam>
+	/// <returns>True if the item has been moved successfully.</returns>
+	public static bool TryMoveItem<T>(this ListBox listBox, int index, int newIndex, Func<IEnumerable, int, int, bool>? moveItemFunc = null, bool scrollIntoView = true)
+	{
+		var itemCount = listBox.ItemCount;
+		if (index < 0 || index >= itemCount || newIndex < 0 || newIndex >= itemCount)
+			return false;
+		if (index != newIndex)
+		{
+			if (listBox.ItemsSource is not { } itemsSource)
+				return false;
+			if (moveItemFunc is null)
+			{
+				if (itemsSource is AvaloniaList<T> avaloniaList)
+				{
+					moveItemFunc = (_, _, _) =>
+					{
+						avaloniaList.Move(index, newIndex);
+						return true;
+					};
+				}
+				else if (itemsSource is ObservableList<T> observableList)
+				{
+					moveItemFunc = (_, _, _) =>
+					{
+						observableList.Move(index, newIndex);
+						return true;
+					};
+				}
+				else if (itemsSource is ObservableCollection<T> observableCollection)
+				{
+					moveItemFunc = (_, _, _) =>
+					{
+						observableCollection.Move(index, newIndex);
+						return true;
+					};
+				}
+				else
+					return false;
+			}
+			var item = listBox.Items[index];
+			var restoreFocus = listBox.SelectedIndex == index && listBox.ContainerFromIndex(index)?.IsFocused == true;
+			if (!moveItemFunc(itemsSource, index, newIndex))
+				return false;
+			listBox.SelectedIndex = newIndex;
+			Dispatcher.UIThread.Post(() =>
+			{
+				if (restoreFocus && listBox.SelectedItem?.Equals(item) == true && listBox.ContainerFromIndex(newIndex) is { } container)
+					container.Focus();
+				if (scrollIntoView)
+					listBox.ScrollIntoView(newIndex);
+			});
+		}
+		return true;
 	}
 }
--- Avalonia/Controls/SelectingItemsControlExtensions.cs
+++ Avalonia/Controls/SelectingItemsControlExtensions.cs
@@ -1,95 +1,94 @@
 ﻿using Avalonia.Controls.Primitives;
 
-namespace CarinaStudio.Controls
+namespace CarinaStudio.Controls;
+
+/// <summary>
+/// Extensions for <see cref="SelectingItemsControl"/>.
+/// </summary>
+public static class SelectingItemsControlExtensions
 {
-    /// <summary>
-    /// Extensions for <see cref="SelectingItemsControl"/>.
-    /// </summary>
-    public static class SelectingItemsControlExtensions
-    {
-		/// <summary>
-		/// Select first item in <see cref="SelectingItemsControl"/>.
-		/// </summary>
-		/// <param name="control"><see cref="SelectingItemsControl"/>.</param>
-		/// <param name="scrollIntoView">True to scroll new selected item into view.</param>
-		public static void SelectFirstItem(this SelectingItemsControl control, bool scrollIntoView = true)
+	/// <summary>
+	/// Select first item in <see cref="SelectingItemsControl"/>.
+	/// </summary>
+	/// <param name="control"><see cref="SelectingItemsControl"/>.</param>
+	/// <param name="scrollIntoView">True to scroll new selected item into view.</param>
+	public static void SelectFirstItem(this SelectingItemsControl control, bool scrollIntoView = true)
+	{
+		var itemCount = control.ItemCount;
+		if (itemCount > 0)
 		{
-			var itemCount = control.ItemCount;
-			if (itemCount > 0)
-			{
-				control.SelectedIndex = 0;
-				if (scrollIntoView)
-					control.ScrollIntoView(0);
-			}
+			control.SelectedIndex = 0;
+			if (scrollIntoView)
+				control.ScrollIntoView(0);
 		}
+	}
 
 
-		/// <summary>
-		/// Select last item in <see cref="SelectingItemsControl"/>.
-		/// </summary>
-		/// <param name="control"><see cref="SelectingItemsControl"/>.</param>
-		/// <param name="scrollIntoView">True to scroll new selected item into view.</param>
-		public static void SelectLastItem(this SelectingItemsControl control, bool scrollIntoView = true)
+	/// <summary>
+	/// Select last item in <see cref="SelectingItemsControl"/>.
+	/// </summary>
+	/// <param name="control"><see cref="SelectingItemsControl"/>.</param>
+	/// <param name="scrollIntoView">True to scroll new selected item into view.</param>
+	public static void SelectLastItem(this SelectingItemsControl control, bool scrollIntoView = true)
+	{
+		var itemCount = control.ItemCount;
+		if (itemCount > 0)
 		{
-			var itemCount = control.ItemCount;
-			if (itemCount > 0)
-			{
-				control.SelectedIndex = itemCount - 1;
-				if (scrollIntoView)
-					control.ScrollIntoView(itemCount - 1);
-			}
+			control.SelectedIndex = itemCount - 1;
+			if (scrollIntoView)
+				control.ScrollIntoView(itemCount - 1);
 		}
+	}
 
 
-		/// <summary>
-		/// Select next item in <see cref="SelectingItemsControl"/>.
-		/// </summary>
-		/// <param name="control"><see cref="SelectingItemsControl"/>.</param>
-		/// <param name="scrollIntoView">True to scroll new selected item into view.</param>
-		/// <returns>New index of selected item.</returns>
-		public static int SelectNextItem(this SelectingItemsControl control, bool scrollIntoView = true)
+	/// <summary>
+	/// Select next item in <see cref="SelectingItemsControl"/>.
+	/// </summary>
+	/// <param name="control"><see cref="SelectingItemsControl"/>.</param>
+	/// <param name="scrollIntoView">True to scroll new selected item into view.</param>
+	/// <returns>New index of selected item.</returns>
+	public static int SelectNextItem(this SelectingItemsControl control, bool scrollIntoView = true)
+	{
+		var itemCount = control.ItemCount;
+		if (itemCount > 0)
 		{
-			var itemCount = control.ItemCount;
-			if (itemCount > 0)
+			var newIndex = control.SelectedIndex;
+			if (newIndex < itemCount - 1)
 			{
-				var newIndex = control.SelectedIndex;
-				if (newIndex < itemCount - 1)
-				{
-					++newIndex;
-					control.SelectedIndex = newIndex;
-					if (scrollIntoView)
-						control.ScrollIntoView(newIndex);
-				}
-				return newIndex;
-			}
-			return control.SelectedIndex;
-		}
-
-
-		/// <summary>
-		/// Select previous item in <see cref="SelectingItemsControl"/>.
-		/// </summary>
-		/// <param name="control"><see cref="SelectingItemsControl"/>.</param>
-		/// <param name="scrollIntoView">True to scroll new selected item into view.</param>
-		/// <returns>New index of selected item.</returns>
-		public static int SelectPreviousItem(this SelectingItemsControl control, bool scrollIntoView = true)
-		{
-			var itemCount = control.ItemCount;
-			if (itemCount > 0)
-			{
-				var newIndex = control.SelectedIndex;
-				if (newIndex < 0)
-					newIndex = itemCount - 1;
-				else if (newIndex > 0)
-					--newIndex;
-				else
-					return newIndex;
+				++newIndex;
 				control.SelectedIndex = newIndex;
 				if (scrollIntoView)
 					control.ScrollIntoView(newIndex);
-				return newIndex;
 			}
-			return control.SelectedIndex;
+			return newIndex;
 		}
+		return control.SelectedIndex;
+	}
+
+
+	/// <summary>
+	/// Select previous item in <see cref="SelectingItemsControl"/>.
+	/// </summary>
+	/// <param name="control"><see cref="SelectingItemsControl"/>.</param>
+	/// <param name="scrollIntoView">True to scroll new selected item into view.</param>
+	/// <returns>New index of selected item.</returns>
+	public static int SelectPreviousItem(this SelectingItemsControl control, bool scrollIntoView = true)
+	{
+		var itemCount = control.ItemCount;
+		if (itemCount > 0)
+		{
+			var newIndex = control.SelectedIndex;
+			if (newIndex < 0)
+				newIndex = itemCount - 1;
+			else if (newIndex > 0)
+				--newIndex;
+			else
+				return newIndex;
+			control.SelectedIndex = newIndex;
+			if (scrollIntoView)
+				control.ScrollIntoView(newIndex);
+			return newIndex;
+		}
+		return control.SelectedIndex;
 	}
 }
